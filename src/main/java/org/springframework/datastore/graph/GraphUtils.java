7b60b405b94f66b33f358389eda018bd790e1c77
trisberg <trisberg@vmware.com>
2010-05-10 15:28:28 -0400

    initial commit for graph project

---
+++ src/main/java/org/springframework/datastore/graph/GraphUtils.java
@@ -0,0 +1,5 @@
+package org.springframework.datastore.graph;
+
+public class GraphUtils {
+
+}
---
+++ src/test/java/org/springframework/datastore/graph/GraphUtilsTests.java
@@ -0,0 +1,13 @@
+package org.springframework.datastore.graph;
+
+import org.junit.Test;
+
+
+public class GraphUtilsTests {
+	
+	@Test
+	public void testSomething() {
+		
+	}
+    
+}
