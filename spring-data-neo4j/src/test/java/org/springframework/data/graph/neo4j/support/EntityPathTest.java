0778566ff5011a366270c7c264a31742a1f4c967
Michael Hunger <github@jexp.de>
2011-03-03 00:40:17 +0100

    tests for entitypath

--- spring-data-neo4j/src/test/java/org/springframework/data/graph/neo4j/support/EntityPathTest.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/graph/neo4j/support/EntityPathTest.java
@@ -1,9 +1,43 @@
 package org.springframework.data.graph.neo4j.support;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.neo4j.graphdb.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.graph.neo4j.Person;
+import org.springframework.data.graph.neo4j.template.NodePath;
+import org.springframework.data.graph.neo4j.transaction.EntityMapper;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @author mh
  * @since 26.02.11
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:org/springframework/data/graph/neo4j/support/Neo4jGraphPersistenceTest-context.xml"})
 public class EntityPathTest {
 
+    @Autowired
+    private GraphDatabaseContext ctx;
+
+    @Test
+    @Transactional
+    public void shouldConvertNodePathToEntityPath() throws Exception {
+        Person michael = new Person("Michael", 36);
+        Node node = michael.getUnderlyingState();
+        NodePath path = new NodePath(node);
+        EntityPath<Person, Person> entityPath = new EntityPath<Person, Person>(ctx, path);
+
+        Assert.assertEquals("start entity",michael, entityPath.startEntity());
+        Assert.assertEquals("start node",node, path.startNode());
+        Assert.assertEquals("end entity",michael, entityPath.endEntity());
+        Assert.assertEquals("end node",node, path.endNode());
+        Assert.assertNull("no relationship", entityPath.lastRelationshipEntity());
+        Assert.assertNull("no relationship", path.lastRelationship());
+
+        // todo all 6 iterators
+    }
 }
--- spring-data-neo4j/src/test/java/org/springframework/data/graph/neo4j/transaction/EntityMapperTest.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/graph/neo4j/transaction/EntityMapperTest.java
@@ -1,8 +1,39 @@
 package org.springframework.data.graph.neo4j.transaction;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.graph.neo4j.Person;
+import org.springframework.data.graph.neo4j.support.EntityPath;
+import org.springframework.data.graph.neo4j.support.GraphDatabaseContext;
+import org.springframework.data.graph.neo4j.template.NodePath;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @author mh
  * @since 26.02.11
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:org/springframework/data/graph/neo4j/support/Neo4jGraphPersistenceTest-context.xml"})
 public class EntityMapperTest {
+
+    @Autowired
+    private GraphDatabaseContext ctx;
+
+    @Test
+    @Transactional
+    public void entityMapperShouldForwardEntityPath() throws Exception {
+        Person michael = new Person("Michael", 36);
+        EntityMapper<Person, Person, String> mapper = new EntityMapper<Person, Person, String>(ctx) {
+            @Override
+            public String mapPath(EntityPath<Person, Person> entityPath) {
+                return entityPath.<Person>startEntity().getName();
+            }
+        };
+        String name = mapper.mapPath(new NodePath(michael.getUnderlyingState()));
+        Assert.assertEquals(michael.getName(), name);
+    }
 }
