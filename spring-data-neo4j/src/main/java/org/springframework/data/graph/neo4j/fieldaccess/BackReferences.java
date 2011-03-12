ef03b7230d39bca48f178f89b1d7d12281bad4e4
Michael Hunger <github@jexp.de>
2011-03-12 22:37:25 +0000

    extracted back-references

---
+++ spring-data-neo4j/src/main/java/org/springframework/data/graph/neo4j/fieldaccess/BackReferences.java
@@ -0,0 +1,44 @@
+package org.springframework.data.graph.neo4j.fieldaccess;
+
+import org.springframework.data.graph.annotation.RelatedTo;
+import org.springframework.data.graph.core.GraphBacked;
+import org.springframework.data.graph.core.NodeBacked;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+/**
+ * @author mh
+ * @since 12.03.11
+ */
+public class BackReferences {
+    private List<NodeBacked> backrefs=new ArrayList<NodeBacked>();
+    private EntityState<?, ?> entityState;
+
+    public BackReferences(EntityState<?,?> entityState) {
+
+        this.entityState = entityState;
+    }
+
+    public void addBackReferences(Collection<NodeBacked> backReference) {
+        this.backrefs.addAll(backReference);
+    }
+
+
+    private void pruneInvalidBackRefs() {
+        GraphBacked entity = entityState.getEntity();
+        for (Iterator<NodeBacked> it = backrefs.iterator(); it.hasNext();) {
+            NodeBacked backRef = it.next();
+            if (backRef.refersTo(entity)) continue;
+            it.remove();
+        }
+    }
+
+
+    public void persistNeighbours() {
+        pruneInvalidBackRefs();
+        for (NodeBacked backref : backrefs) {
+            backref.persist();
+        }
+    }
+}
