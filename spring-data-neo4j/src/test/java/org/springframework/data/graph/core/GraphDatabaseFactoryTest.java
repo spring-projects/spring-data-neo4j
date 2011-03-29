3348eec4e6ef1c633446d38985863c61af240cad
Michael Hunger <github@jexp.de>
2011-03-29 22:04:32 +0200

    Test for GraphDatabaseFactory

---
+++ spring-data-neo4j/src/test/java/org/springframework/data/graph/core/GraphDatabaseFactoryTest.java
@@ -0,0 +1,43 @@
+package org.springframework.data.graph.core;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.graph.neo4j.support.LocalGraphDatabase;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author mh
+ * @since 29.03.11
+ */
+public class GraphDatabaseFactoryTest {
+
+    @Test
+    public void shouldCreateLocalDatabaseFromContext() throws Exception {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("GraphDatabaseFactory-context.xml");
+        try {
+            GraphDatabase graphDatabase = ctx.getBean("graphDatabase", GraphDatabase.class);
+            assertThat(graphDatabase, is(not(nullValue())));
+            assertThat(graphDatabase, is(instanceOf(LocalGraphDatabase.class)));
+        } finally {
+            ctx.close();
+        }
+
+    }
+    @Test
+    public void shouldCreateLocalDatabase() throws Exception {
+        GraphDatabaseFactory factory = new GraphDatabaseFactory();
+        try {
+            factory.setStoreLocation("target/test-db");
+            GraphDatabase graphDatabase = factory.getObject();
+            assertThat(graphDatabase, is(not(nullValue())));
+            assertThat(graphDatabase,is(instanceOf(LocalGraphDatabase.class)));
+        } finally {
+            factory.shutdown();
+        }
+    }
+}
