f081e0373d60257eb2bed64b5364605d13f8098f
Michael Hunger <github@jexp.de>
2011-09-07 13:57:52 +0200

    started adding GremlinQueryEngine Tests for embedded and REST, some workarounds for expected results in the form Iterable<Map<String,Object>> (similar to cypher)

--- spring-data-neo4j-rest/src/main/java/org/springframework/data/neo4j/rest/RestGremlinQueryEngine.java
+++ spring-data-neo4j-rest/src/main/java/org/springframework/data/neo4j/rest/RestGremlinQueryEngine.java
@@ -44,7 +44,7 @@ public class RestGremlinQueryEngine implements QueryEngine<Object> {
 
     @Override
     public QueryResult<Object> query(String statement, Map<String, Object> params) {
-        final String paramsString = JsonHelper.createJsonFrom(params);
+        final String paramsString = JsonHelper.createJsonFrom(params == null ? Collections.emptyMap() : params);
         final String data = JsonHelper.createJsonFrom(MapUtil.map("script", statement,"params",paramsString));
         final RequestResult requestResult = restRequest.get("ext/GremlinPlugin/graphdb/execute_script", data);
         final Object result = JsonHelper.readJson(requestResult.getEntity());
