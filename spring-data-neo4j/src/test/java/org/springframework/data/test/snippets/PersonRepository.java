f6af7ece70618c77a10b8275d4a91d9541229597
Michael Hunger <github@jexp.de>
2011-10-14 14:41:18 +0200

    added documeting test for derived queries

---
+++ spring-data-neo4j/src/test/java/org/springframework/data/test/snippets/PersonRepository.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.test.snippets;
+
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+/**
+* @author mh
+* @since 14.10.11
+*/
+public interface PersonRepository extends GraphRepository<SnippetRepositoryDerivedFinderTest.Person> {
+    Iterable<SnippetRepositoryDerivedFinderTest.Person> findByGroupTitle(String name);
+}
