249e845f2763fe2366196052fc8c1a80056bac85
Andres Taylor <andres@neotechnology.com>
2011-10-21 15:09:35 +0200

    Non-functional state of cineasts-rest

--- spring-data-neo4j-examples/cineasts-rest/src/main/java/org/neo4j/cineasts/repository/MovieRepository.java
+++ spring-data-neo4j-examples/cineasts-rest/src/main/java/org/neo4j/cineasts/repository/MovieRepository.java
@@ -1,6 +1,9 @@
 package org.neo4j.cineasts.repository;
 
 import org.neo4j.cineasts.domain.Movie;
+import org.springframework.data.neo4j.annotation.MapResult;
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.neo4j.annotation.ResultColumn;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.neo4j.repository.NamedIndexRepository;
 
@@ -9,4 +12,19 @@ import org.springframework.data.neo4j.repository.NamedIndexRepository;
  * @since 02.04.11
  */
 public interface MovieRepository extends GraphRepository<Movie>, NamedIndexRepository<Movie> {
+
+    @Query("START movie=node({0}) MATCH movie-[rating?:rating]->() RETURN movie, AVG(rating)")
+    MovieData getMovieData(Long movieId);
+
+    @MapResult
+    interface MovieData {
+        @ResultColumn("movie")
+        Movie getMovie();
+
+        @ResultColumn("avg(rating.stars)")
+        Integer getAverageRating();
+    }
+
 }
+
+
