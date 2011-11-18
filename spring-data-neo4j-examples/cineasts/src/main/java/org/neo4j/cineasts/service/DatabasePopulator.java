ce57f6c37dc1af6c42b6ebf1c6f661695d97229d
Michael Hunger <github@jexp.de>
2011-11-18 04:36:25 +0100

    updated documentation

--- spring-data-neo4j-examples/cineasts/src/main/java/org/neo4j/cineasts/service/DatabasePopulator.java
+++ spring-data-neo4j-examples/cineasts/src/main/java/org/neo4j/cineasts/service/DatabasePopulator.java
@@ -35,8 +35,9 @@ public class DatabasePopulator {
     @Transactional
     public List<Movie> populateDatabase() {
         User me = userRepository.save(new User("micha", "Micha", "password", User.Roles.ROLE_ADMIN,User.Roles.ROLE_USER));
-        User ollie = userRepository.save(new User("ollie", "Olliver", "password",User.Roles.ROLE_USER));
+        User ollie = new User("ollie", "Olliver", "password",User.Roles.ROLE_USER);
         me.addFriend(ollie);
+        userRepository.save(me);
         List<Integer> ids = asList(19995 , 194, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 13, 20526, 11, 1893, 1892, 1894, 168, 193, 200, 157, 152, 201, 154, 12155, 58, 285, 118, 22, 392, 5255, 568, 9800, 497, 101, 120, 121, 122);
         List<Movie> result=new ArrayList<Movie>(ids.size());
         for (Integer id : ids) {
