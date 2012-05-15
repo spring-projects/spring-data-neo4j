36ce5baf26a584d6059108f2f2ffcbd7f284d361
loki2302 <loki@ubuntu.(none)>
2012-05-15 12:24:20 -0700

    DATAGRAPH-175: updated refactored hello-worlds-aspects example to make it easier to interprete

--- spring-data-neo4j-examples/hello-worlds-aspects/src/main/java/org/springframework/data/neo4j/examples/hellograph/RelationshipTypes.java
+++ spring-data-neo4j-examples/hello-worlds-aspects/src/main/java/org/springframework/data/neo4j/examples/hellograph/RelationshipTypes.java
@@ -1,6 +1,5 @@
 package org.springframework.data.neo4j.examples.hellograph;
 
-public abstract class RelationshipTypes
-{
+public abstract class RelationshipTypes {
     public static final String REACHABLE_BY_ROCKET = "REACHABLE_BY_ROCKET";
 }
