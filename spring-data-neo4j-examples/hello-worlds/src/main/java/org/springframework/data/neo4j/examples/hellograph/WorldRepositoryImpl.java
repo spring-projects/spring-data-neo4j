2fd22d77f6342de528a8a1916e68546a99b64afa
loki2302 <loki@ubuntu.(none)>
2012-05-15 12:42:10 -0700

    DATAGRAPH-175: refactored hello-worlds example to make it easier to interprete

--- spring-data-neo4j-examples/hello-worlds/src/main/java/org/springframework/data/neo4j/examples/hellograph/WorldRepositoryImpl.java
+++ spring-data-neo4j-examples/hello-worlds/src/main/java/org/springframework/data/neo4j/examples/hellograph/WorldRepositoryImpl.java
@@ -1,65 +1,46 @@
 package org.springframework.data.neo4j.examples.hellograph;
 
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.traversal.TraversalDescription;
-import org.neo4j.kernel.Traversal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-import static org.neo4j.graphdb.DynamicRelationshipType.withName;
-import static org.springframework.data.neo4j.examples.hellograph.RelationshipTypes.REACHABLE_BY_ROCKET;
 
 /**
  * Spring Data Neo4j backed application context for Worlds.
  */
 public class WorldRepositoryImpl implements MyWorldRepository {
 
-    @Autowired private WorldRepository worldRepository;
+    @Autowired
+    private WorldRepository worldRepository;
 
     @Override
     @Transactional
-    public Collection<World> makeSomeWorlds() {
-        ArrayList<World> newWorlds = new ArrayList<World>();
-
-        // solar worlds
-        newWorlds.add(world("Mercury", 0));
-        newWorlds.add(world("Venus", 0));
-        World earth = world("Earth", 1);
-        newWorlds.add(earth);
+    public void makeSureGalaxyIsNotEmpty() {
+        // Solar worlds
+        resolveWorld("Mercury", 0);
+        resolveWorld("Venus", 0);
         
-        World mars = world("Mars", 2);
-        earth.addRocketRouteTo(mars);
-        worldRepository.save(earth);
+        World earth = resolveWorld("Earth", 1);        
+        World mars = resolveWorld("Mars", 2);        
+        mars.addRocketRouteTo(earth);
         
-        newWorlds.add(mars);
-        newWorlds.add(world("Jupiter", 63));
-        newWorlds.add(world("Saturn", 62));
-        newWorlds.add(world("Uranus", 27));
-        newWorlds.add(world("Neptune", 13));
+        resolveWorld("Jupiter", 63);
+        resolveWorld("Saturn", 62);
+        resolveWorld("Uranus", 27);
+        resolveWorld("Neptune", 13);
 
         // Norse worlds
-        newWorlds.add(world("Alfheimr", 0));
-        newWorlds.add(world("Midgard", 1));
-        newWorlds.add(world("Muspellheim", 2));
-        newWorlds.add(world("Asgard", 63));
-        newWorlds.add(world("Hel", 62));
-        
-        return newWorlds;
+        resolveWorld("Alfheimr", 0);
+        resolveWorld("Midgard", 1);
+        resolveWorld("Muspellheim", 2);
+        resolveWorld("Asgard", 63);
+        resolveWorld("Hel", 62);        
     }
-
-
-    @Override
-    @Transactional
-    public World world(String name, int moons) {
-    	
-        World createdWorld = findWorldNamed(name);
-        if (createdWorld == null) {
-        	createdWorld = new World(name, moons);
-        	worldRepository.save(createdWorld);
-        }
+    
+    private World resolveWorld(String name, int moons) {
+    	World createdWorld = findWorldNamed(name);
+    	if (createdWorld == null) {
+    		createdWorld = new World(name, moons);
+    		worldRepository.save(createdWorld);
+    	}
     	return createdWorld;
     }
 
@@ -67,16 +48,9 @@ public class WorldRepositoryImpl implements MyWorldRepository {
     public World findWorldNamed(String name) {
         return worldRepository.findByPropertyValue("name", name);
     }
-
+    
     @Override
-    public Iterable<World> findWorldsWithMoons(int moonCount) {
-        return worldRepository.findAllByPropertyValue("moon-index", "moons", moonCount);
+    public Iterable<World> findAllWorlds() {
+    	return worldRepository.findAll();
     }
-
-    @Override
-    public Iterable<World> exploreWorldsBeyond(World homeWorld) {
-        TraversalDescription traversal = Traversal.description().relationships(withName(REACHABLE_BY_ROCKET), Direction.OUTGOING);
-        return worldRepository.findAllByTraversal(homeWorld, traversal);
-    }
-
 }
