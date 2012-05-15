ee1c19ceb80b07bdb783e13a5b84f50804428a31
loki2302 <loki@ubuntu.(none)>
2012-05-15 12:42:10 -0700

    DATAGRAPH-175: refactored hello-worlds example to make it easier to interprete

--- spring-data-neo4j-examples/hello-worlds/src/main/java/org/springframework/data/neo4j/examples/hellograph/World.java
+++ spring-data-neo4j-examples/hello-worlds/src/main/java/org/springframework/data/neo4j/examples/hellograph/World.java
@@ -1,72 +1,55 @@
 package org.springframework.data.neo4j.examples.hellograph;
 
-
 import org.neo4j.graphdb.Direction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.annotation.Fetch;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.Indexed;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.RelatedTo;
-import org.springframework.data.neo4j.support.Neo4jTemplate;
-
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * A Spring Data Neo4j enhanced World entity.
- * <p/>
- * This is the initial POJO in the Universe.
- */
 @NodeEntity
-public class World 
-{   
-    @GraphId Long id;
+public class World {   
+    @GraphId
+    Long id;
     
     @Indexed
     private String name;
 
-    @Indexed(indexName = "moon-index")
     private int moons;
 
     @Fetch
-    @RelatedTo(type = "REACHABLE_BY_ROCKET", direction = Direction.BOTH)
+    @RelatedTo(type = RelationshipTypes.REACHABLE_BY_ROCKET, direction = Direction.BOTH)
     Set<World> reachableByRocket = new HashSet<World>();
 
-    public World( String name, int moons )
-    {
+    public World(String name, int moons) {
         this.name = name;
         this.moons = moons;
     }
 
-    public World()
-    {
+    public World() {
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public int getMoons()
-    {
+    public int getMoons() {
         return moons;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return String.format("World{name='%s, moons=%d}", name, moons);
     }
 
-    public void addRocketRouteTo( World otherWorld )
-    {
+    public void addRocketRouteTo(World otherWorld) {
     	reachableByRocket.add(otherWorld);
     }
 
-    public boolean canBeReachedFrom( World otherWorld )
-    {
-        return reachableByRocket.contains( otherWorld );
+    public Iterable<World> getReachableWorlds() {
+    	return reachableByRocket;
     }
 
 	@Override
