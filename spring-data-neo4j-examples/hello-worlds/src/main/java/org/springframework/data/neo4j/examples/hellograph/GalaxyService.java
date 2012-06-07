c0fd916780118f9d0c4006afc34f6f87f62aca75
Andrey <loki@ubuntu.(none)>
2012-06-07 22:07:39 +0400

    Removed DTO and mapping, reverted tests.

--- spring-data-neo4j-examples/hello-worlds/src/main/java/org/springframework/data/neo4j/examples/hellograph/GalaxyService.java
+++ spring-data-neo4j-examples/hello-worlds/src/main/java/org/springframework/data/neo4j/examples/hellograph/GalaxyService.java
@@ -1,65 +1,68 @@
 package org.springframework.data.neo4j.examples.hellograph;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GalaxyService {
+	
 	@Autowired
 	private WorldRepository worldRepository;
 	
-	@Autowired
-	private GalaxyMapper mapper;
+	public long getNumberOfWorlds() {
+		return worldRepository.count();
+	}
 	
-    @Transactional
-    public void makeSureGalaxyIsNotEmpty() {    	    	
-        // Solar worlds
-        resolveWorld("Mercury", 0);
-        resolveWorld("Venus", 0);
+	public World createWorld(String name, int moons) {
+		return worldRepository.save(new World(name, moons));
+	}
+	
+	public Iterable<World> getAllWorlds() {
+		return worldRepository.findAll();
+	}
+	
+	public World findWorldById(Long id) {
+		return worldRepository.findOne(id);
+	}
+	
+	public World findWorldByName(String name) {
+		return worldRepository.findByPropertyValue("name", name);
+	}
+	
+	public Iterable<World> findAllByNumberOfMoons(int numberOfMoons) {
+		return worldRepository.findAllByPropertyValue("moons", numberOfMoons);
+	}
+	
+	public Collection<World> makeSomeWorlds() {
+		Collection<World> worlds = new ArrayList<World>();
+				
+		// Solar worlds		
+		worlds.add(createWorld("Mercury", 0));
+		worlds.add(createWorld("Venus", 0));
         
-        World earth = resolveWorld("Earth", 1);        
-        World mars = resolveWorld("Mars", 2);        
+        World earth = createWorld("Earth", 1);        
+        World mars = createWorld("Mars", 2);        
         mars.addRocketRouteTo(earth);
         worldRepository.save(mars);
+        worlds.add(earth);
+        worlds.add(mars);
         
-        resolveWorld("Jupiter", 63);
-        resolveWorld("Saturn", 62);
-        resolveWorld("Uranus", 27);
-        resolveWorld("Neptune", 13);
+        worlds.add(createWorld("Jupiter", 63));
+        worlds.add(createWorld("Saturn", 62));
+        worlds.add(createWorld("Uranus", 27));
+        worlds.add(createWorld("Neptune", 13));
 
         // Norse worlds
-        resolveWorld("Alfheimr", 0);
-        resolveWorld("Midgard", 1);
-        resolveWorld("Muspellheim", 2);
-        resolveWorld("Asgard", 63);
-        resolveWorld("Hel", 62);        
-    }
-    
-    private World resolveWorld(String name, int moons) {
-    	World createdWorld = worldRepository.findByPropertyValue("name", name);
-    	if (createdWorld == null) {
-    		createdWorld = new World(name, moons);
-    		createdWorld = worldRepository.save(createdWorld);    		 
-    	}
-    	
-    	return createdWorld;
-    }
-
-    public WorldDto findWorldNamed(String name) {
-    	World world = worldRepository.findByPropertyValue("name", name);
-        return mapper.worldDtoFromWorld(world);
-    }
-    
-    public List<WorldDto> findReachableWorlds(Long worldId) {
-    	World world = worldRepository.findOne(worldId);
-    	return mapper.worldDtosFromWorlds(world.getReachableWorlds());
-    }
-    
-    public List<WorldDto> findAllWorlds() {
-    	Iterable<World> worlds = worldRepository.findAll();
-    	return mapper.worldDtosFromWorlds(worlds);
-    }
+        worlds.add(createWorld("Alfheimr", 0));
+        worlds.add(createWorld("Midgard", 1));
+        worlds.add(createWorld("Muspellheim", 2));
+        worlds.add(createWorld("Asgard", 63));
+        worlds.add(createWorld("Hel", 62));
+        
+        return worlds;
+	}
+	
 }
