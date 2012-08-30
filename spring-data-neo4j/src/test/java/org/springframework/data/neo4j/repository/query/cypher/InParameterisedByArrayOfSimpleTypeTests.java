3ba54fd4001282bef79f420a48715701b1eccce0
Lasse Westh-Nielsen <lassewesth@gmail.com>
2012-08-30 11:26:00 +0100

    correcting tests

--- spring-data-neo4j/src/test/java/org/springframework/data/neo4j/repository/query/cypher/InParameterisedByArrayOfSimpleTypeTests.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/repository/query/cypher/InParameterisedByArrayOfSimpleTypeTests.java
@@ -16,7 +16,6 @@
 package org.springframework.data.neo4j.repository.query.cypher;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -44,73 +43,73 @@ class SimpleTypeArrayEntity {
     @GraphId
     Long id;
 
-    boolean[] bo = {false, false, false};
-    byte[] by = "Hello, world!\n".getBytes();
-    short[] sh = {1, 2, 3};
-    int[] i = {4, 5, 6};
-    long[] l = {7,8,9};
-    float[] f = {1.0f, 1.1f, 1.2f};
-    double[] d = {1.3, 1.4, 1.5};
-    char[] c = "Hello Darkness, my old friend...".toCharArray();
-    String[] st = {"bigger", "better", "faster", "stronger"};
+    boolean[] booleans = {false, false, false};
+    byte[] bytes = "Hello, world!\n".getBytes();
+    short[] shorts = {1, 2, 3};
+    int[] ints = {4, 5, 6};
+    long[] longs = {7,8,9};
+    float[] floats = {1.0f, 1.1f, 1.2f};
+    double[] doubles = {1.3, 1.4, 1.5};
+    char[] chars = "Hello Darkness, my old friend...".toCharArray();
+    String[] strings = {"bigger", "better", "faster", "stronger"};
 
     SimpleTypeArrayEntity() {
     }
 
-    SimpleTypeArrayEntity(boolean[] bo, byte[] by, short[] sh, int[] i, long[] l, float[] f, double[] d, char[] c, String[] st) {
-        this.bo = bo;
-        this.by = by;
-        this.sh = sh;
-        this.i = i;
-        this.l = l;
-        this.f = f;
-        this.d = d;
-        this.c = c;
-        this.st = st;
+    SimpleTypeArrayEntity(boolean[] booleans, byte[] bytes, short[] shorts, int[] ints, long[] longs, float[] floats, double[] doubles, char[] chars, String[] strings) {
+        this.booleans = booleans;
+        this.bytes = bytes;
+        this.shorts = shorts;
+        this.ints = ints;
+        this.longs = longs;
+        this.floats = floats;
+        this.doubles = doubles;
+        this.chars = chars;
+        this.strings = strings;
     }
 }
 
 interface SimpleTypeArrayEntityRepository extends CRUDRepository<SimpleTypeArrayEntity> {
-    @Query("start n=node(*) where n.bo in {0} return n")
+    @Query("start n=node(*) where n.booleans in {0} return n")
     SimpleTypeArrayEntity findUsingBoolean(Iterable<boolean[]> value);
 
-    @Query("start n=node(*) where n.by in {0} return n")
+    @Query("start n=node(*) where n.bytes in {0} return n")
     SimpleTypeArrayEntity findUsingByte(Iterable<byte[]> value);
 
-    @Query("start n=node(*) where n.sh in {0} return n")
+    @Query("start n=node(*) where n.shorts in {0} return n")
     SimpleTypeArrayEntity findUsingShort(Iterable<short[]> value);
 
-    @Query("start n=node(*) where n.i in {0} return n")
+    @Query("start n=node(*) where n.ints in {0} return n")
     SimpleTypeArrayEntity findUsingInt(Iterable<int[]> value);
 
-    @Query("start n=node(*) where n.l in {0} return n")
+    @Query("start n=node(*) where n.longs in {0} return n")
     SimpleTypeArrayEntity findUsingLong(Iterable<long[]> value);
 
-    @Query("start n=node(*) where n.f in {0} return n")
+    @Query("start n=node(*) where n.floats in {0} return n")
     SimpleTypeArrayEntity findUsingFloat(Iterable<float[]> value);
 
-    @Query("start n=node(*) where n.d in {0} return n")
+    @Query("start n=node(*) where n.doubles in {0} return n")
     SimpleTypeArrayEntity findUsingDouble(Iterable<double[]> value);
 
-    @Query("start n=node(*) where n.c in {0} return n")
+    @Query("start n=node(*) where n.chars in {0} return n")
     SimpleTypeArrayEntity findUsingChar(Iterable<char[]> value);
 
-    @Query("start n=node(*) where n.st in {0} return n")
+    @Query("start n=node(*) where n.strings in {0} return n")
     SimpleTypeArrayEntity findUsingString(Iterable<String[]> value);
 }
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class InParameterisedByArrayOfSimpleTypeTests {
-    public static final byte[] BY = new byte[]{0x42, 0x43};
-    public static final boolean[] BO = new boolean[]{true, false};
-    public static final short[] S = new short[]{23, 24};
-    public static final int[] I = new int[]{63, 64};
-    public static final long[] L = new long[]{87l, 88l};
-    public static final float[] F = new float[]{3.14f, 3.15f};
-    public static final double[] D = new double[]{2.97, 2.98};
-    public static final char[] C = new char[]{'!', '@'};
-    public static final String[] ST = new String[]{"dub", "idub"};
+    public static final byte[] BYTES = new byte[]{0x42, 0x43};
+    public static final boolean[] BOOLEANS = new boolean[]{true, false};
+    public static final short[] SHORTS = new short[]{23, 24};
+    public static final int[] INTS = new int[]{63, 64};
+    public static final long[] LONGS = new long[]{87l, 88l};
+    public static final float[] FLOATS = new float[]{3.14f, 3.15f};
+    public static final double[] DOUBLES = new double[]{2.97, 2.98};
+    public static final char[] CHARS = new char[]{'!', '@'};
+    public static final String[] STRINGS = new String[]{"dub", "idub"};
 
     @Configuration
     @EnableNeo4jRepositories
@@ -135,7 +134,7 @@ public class InParameterisedByArrayOfSimpleTypeTests {
 
         try {
             simpleTypeArrayEntityRepository.save(new SimpleTypeArrayEntity());
-            simpleTypeArrayEntityRepository.save(new SimpleTypeArrayEntity(BO, BY, S, I, L, F, D, C, ST));
+            simpleTypeArrayEntityRepository.save(new SimpleTypeArrayEntity(BOOLEANS, BYTES, SHORTS, INTS, LONGS, FLOATS, DOUBLES, CHARS, STRINGS));
 
             transaction.success();
         } finally {
@@ -145,47 +144,46 @@ public class InParameterisedByArrayOfSimpleTypeTests {
 
     @Test
     public void shouldSupportStringArraysAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingString(asList(ST, new String[]{"foo"})).st, is(ST));
+        assertThat(simpleTypeArrayEntityRepository.findUsingString(asList(STRINGS, new String[]{"foo"})).strings, is(STRINGS));
     }
 
     @Test
     public void shouldSupportBooleanArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingBoolean(asList(BO)).bo, is(BO));
+        assertThat(simpleTypeArrayEntityRepository.findUsingBoolean(asList(BOOLEANS)).booleans, is(BOOLEANS));
     }
 
     @Test
-    @Ignore("see https://github.com/neo4j/community/issues/788")
     public void shouldSupportByteArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingByte(asList(BY)).by, is(BY));
+        assertThat(simpleTypeArrayEntityRepository.findUsingByte(asList(BYTES)).bytes, is(BYTES));
     }
 
     @Test
     public void shouldSupportShortArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingShort(asList(S)).sh, is(S));
+        assertThat(simpleTypeArrayEntityRepository.findUsingShort(asList(SHORTS)).shorts, is(SHORTS));
     }
 
     @Test
     public void shouldSupportIntArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingInt(asList(I)).i, is(I));
+        assertThat(simpleTypeArrayEntityRepository.findUsingInt(asList(INTS)).ints, is(INTS));
     }
 
     @Test
     public void shouldSupportLongArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingLong(asList(L)).l, is(L));
+        assertThat(simpleTypeArrayEntityRepository.findUsingLong(asList(LONGS)).longs, is(LONGS));
     }
 
     @Test
     public void shouldSupportFloatArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingFloat(asList(F)).f, is(F));
+        assertThat(simpleTypeArrayEntityRepository.findUsingFloat(asList(FLOATS)).floats, is(FLOATS));
     }
 
     @Test
     public void shouldSupportDoubleArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingDouble(asList(D)).d, is(D));
+        assertThat(simpleTypeArrayEntityRepository.findUsingDouble(asList(DOUBLES)).doubles, is(DOUBLES));
     }
 
     @Test
     public void shouldSupportCharArrayAsParameterForIn() throws Exception {
-        assertThat(simpleTypeArrayEntityRepository.findUsingChar(asList(C)).c, is(C));
+        assertThat(simpleTypeArrayEntityRepository.findUsingChar(asList(CHARS)).chars, is(CHARS));
     }
 }
