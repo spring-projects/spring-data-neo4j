9c08245389a87057e6a0512e28dff0f667e99edb
Luanne Misquitta <luanne.coutinho@gmail.com>
2015-03-13 15:01:56 +0530

    Entity access tests exercising relationship entities

---
+++ neo4j-ogm/src/test/java/org/neo4j/ogm/defects/defaultEntityAccessStrategy/AnnotatedFieldNonAnnotatedSetterAndNonSetter.java
@@ -0,0 +1,101 @@
+package org.neo4j.ogm.defects.defaultEntityAccessStrategy;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.neo4j.ogm.annotation.EndNode;
+import org.neo4j.ogm.annotation.Relationship;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
+import org.neo4j.ogm.entityaccess.DefaultEntityAccessStrategy;
+import org.neo4j.ogm.entityaccess.EntityAccess;
+import org.neo4j.ogm.entityaccess.FieldWriter;
+import org.neo4j.ogm.metadata.info.ClassInfo;
+import org.neo4j.ogm.metadata.info.DomainInfo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@Ignore
+public class AnnotatedFieldNonAnnotatedSetterAndNonSetter {
+    private DefaultEntityAccessStrategy entityAccessStrategy = new DefaultEntityAccessStrategy();
+    private DomainInfo domainInfo = new DomainInfo("org.neo4j.ogm.defects");
+
+
+    @Test
+    public void shouldPreferAnnotatedFieldOverNonAnnotatedSetterAndNonSetter() {
+        ClassInfo classInfo = this.domainInfo.getClass(End.class.getName());
+        Set<? extends RelEntity> parameter = new HashSet<>();
+
+        EntityAccess objectAccess = this.entityAccessStrategy.getRelationalWriter(classInfo, "REL_ENTITY_TYPE", parameter);
+        assertNotNull("The resultant object accessor shouldn't be null", objectAccess);
+        assertTrue("The access mechanism should be via the field", objectAccess instanceof FieldWriter);
+        End end = new End();
+        objectAccess.write(end, parameter);
+        assertEquals(end.getRelEntities(), parameter);
+    }
+
+    @RelationshipEntity(type="REL_ENTITY_TYPE")
+    public static class RelEntity {
+        Long id;
+        @StartNode
+        Start start;
+        @EndNode
+        End end;
+
+        public RelEntity() {
+        }
+
+        public End getEnd() {
+            return end;
+        }
+
+        public void setEnd(End end) {
+            this.end = end;
+        }
+
+        public Start getStart() {
+            return start;
+        }
+
+        public void setStart(Start start) {
+            this.start = start;
+        }
+    }
+
+    public static class Start {
+        Long id;
+        String name;
+        @Relationship(type = "REL_ENTITY_TYPE", direction = "OUTGOING")
+        Set<RelEntity> relEntities;
+
+        public Start() {
+        }
+    }
+
+    public static class End {
+        Long id;
+        String name;
+        @Relationship(type = "REL_ENTITY_TYPE", direction="INCOMING")
+        Set<RelEntity> relEntities=new HashSet<>();
+
+        public End() {
+        }
+
+        public Set<RelEntity> getRelEntities() {
+            return relEntities;
+        }
+
+        public void setRelEntities(Set<RelEntity> relEntities) {
+            this.relEntities = relEntities;
+        }
+
+        public void addRelEntity(RelEntity relEntity) {
+            relEntities.add(relEntity);
+        }
+
+    }
+}
---
+++ neo4j-ogm/src/test/java/org/neo4j/ogm/unit/entityaccess/relationshipEntities/AnnotatedFieldAndSetterWithNonSetter.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2014-2015 "GraphAware"
+ *
+ * GraphAware Ltd
+ *
+ * This file is part of Neo4j-OGM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.neo4j.ogm.unit.entityaccess.relationshipEntities;
+
+import org.junit.Test;
+import org.neo4j.ogm.annotation.EndNode;
+import org.neo4j.ogm.annotation.Relationship;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
+import org.neo4j.ogm.entityaccess.DefaultEntityAccessStrategy;
+import org.neo4j.ogm.entityaccess.EntityAccess;
+import org.neo4j.ogm.entityaccess.MethodWriter;
+import org.neo4j.ogm.metadata.info.ClassInfo;
+import org.neo4j.ogm.metadata.info.DomainInfo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class AnnotatedFieldAndSetterWithNonSetter {
+    private DefaultEntityAccessStrategy entityAccessStrategy = new DefaultEntityAccessStrategy();
+    private DomainInfo domainInfo = new DomainInfo("org.neo4j.ogm.unit.entityaccess.relationshipEntities");
+
+
+    @Test
+    public void shouldPreferAnnotatedSetterOverAnnotatedFieldAndNonSetter() {
+        ClassInfo classInfo = this.domainInfo.getClass(End.class.getName());
+        Set<RelEntity> parameter = new HashSet<>();
+        parameter.add(new RelEntity());
+
+        EntityAccess objectAccess = this.entityAccessStrategy.getRelationalWriter(classInfo, "REL_ENTITY_TYPE", parameter);
+        assertNotNull("The resultant object accessor shouldn't be null", objectAccess);
+        assertTrue("The access mechanism should be via the method", objectAccess instanceof MethodWriter);
+        End end = new End();
+        objectAccess.write(end, parameter);
+        assertEquals(end.getRelEntities(), parameter);
+    }
+
+    @RelationshipEntity(type="REL_ENTITY_TYPE")
+    public static class RelEntity {
+        Long id;
+        @StartNode
+        Start start;
+        @EndNode
+        End end;
+
+        public RelEntity() {
+        }
+
+        public End getEnd() {
+            return end;
+        }
+
+        public void setEnd(End end) {
+            this.end = end;
+        }
+
+        public Start getStart() {
+            return start;
+        }
+
+        public void setStart(Start start) {
+            this.start = start;
+        }
+    }
+
+    public static class Start {
+        Long id;
+        String name;
+        @Relationship(type = "REL_ENTITY_TYPE", direction = "OUTGOING")
+        Set<RelEntity> relEntities;
+
+        public Start() {
+        }
+    }
+
+    public static class End {
+        Long id;
+        String name;
+        @Relationship(type = "REL_ENTITY_TYPE", direction="INCOMING")
+        Set<RelEntity> relEntities;
+
+        public End() {
+        }
+
+        public Set<RelEntity> getRelEntities() {
+            return relEntities;
+        }
+
+        public void addRelEntity(RelEntity relEntity) {
+
+        }
+
+        @Relationship(type = "REL_ENTITY_TYPE", direction="INCOMING")
+        public void setRelEntities(Set<RelEntity> relEntities) {
+            this.relEntities = relEntities;
+        }
+
+
+    }
+}
