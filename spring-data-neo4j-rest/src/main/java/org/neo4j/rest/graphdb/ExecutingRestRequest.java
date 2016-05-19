da9a434a8382d503bb78609629884cb09206675b
Vince Bickers <vince@mambofish.org>
2016-05-19 11:53:32 +0100

    DATAGRAPH-869 - Support disabling SSL handshaking.

--- spring-data-neo4j-rest/src/main/java/org/neo4j/rest/graphdb/ExecutingRestRequest.java
+++ spring-data-neo4j-rest/src/main/java/org/neo4j/rest/graphdb/ExecutingRestRequest.java
@@ -19,6 +19,17 @@
  */
 package org.neo4j.rest.graphdb;
 
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+import javax.net.ssl.*;
+import javax.ws.rs.core.MediaType;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -31,14 +42,6 @@ import org.neo4j.rest.graphdb.util.JsonHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.core.MediaType;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.util.Map;
-
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 
 public class ExecutingRestRequest implements RestRequest {
@@ -49,6 +52,8 @@ public class ExecutingRestRequest implements RestRequest {
     private final String baseUri;
     private final UserAgent userAgent = new UserAgent();
     private final Client client;
+
+    private static boolean sslHandshakeEnabled = true;
 
     public ExecutingRestRequest( String baseUri ) {
         this( baseUri, null, null );
@@ -67,10 +72,16 @@ public class ExecutingRestRequest implements RestRequest {
     }
 
     protected Client createClient() {
+
+        if (Config.ignoreSSLHandshake() && sslHandshakeEnabled) {
+            disableCertificateValidation();
+        }
+
         Client client = Client.create();
         client.setConnectTimeout(Config.getConnectTimeout());
         client.setReadTimeout(Config.getReadTimeout());
         client.setChunkedEncodingSize(8*1024);
+
         userAgent.install(client);
         if (Config.useLoggingFilter()) {
             client.addFilter(new LoggingFilter());
@@ -168,7 +179,37 @@ public class ExecutingRestRequest implements RestRequest {
         }
     }
 
- 
+
+    // Create a trust manager that does not validate certificate chains
+    private static void disableCertificateValidation() {
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }};
+
+        // Ignore differences between given hostname and certificate hostname
+        HostnameVerifier hv = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) { return true; }
+        };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(hv);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        sslHandshakeEnabled = false;
+        log.warn("Certificate validation has been disabled");
+    }
+
     @Override
     public String getUri() {
         return baseUri;
