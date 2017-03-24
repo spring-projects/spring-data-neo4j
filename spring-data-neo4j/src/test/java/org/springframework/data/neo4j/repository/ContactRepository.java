b5b03097df7b8c1c31bb28b14f4a8fe0575f0372
Mark Angrish <mark.angrish@gmail.com>
2017-03-24 12:08:50 +1100

    DATAGRAPH-976 - Polishing.
    - Fixed broken tests caused by changes in Spring Core and Spring Data.

---
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/repository/ContactRepository.java
@@ -0,0 +1,8 @@
+package org.springframework.data.neo4j.repository;
+
+/**
+ * Created by markangrish on 24/03/2017.
+ */
+public interface ContactRepository extends Neo4jRepository<Contact, String> {
+
+}
