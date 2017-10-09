188c165174b347c72b14de043ccedaef7e6e9849
Gerrit Meier <gerrit.meier@neo4j.com>
2017-10-09 15:17:41 +0200

    DATAGRAPH-791 - support named queries in properties files.
    Neo4j now works the same as other Spring Data projects by supporting
    named queries in `neo4j-named-queries.properties` (default name) files.
    
    (cherry picked from commit be08ec9)

---
+++ spring-data-neo4j/src/main/java/org/springframework/data/neo4j/repository/query/NamedGraphRepositoryQuery.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c)  [2011-2017] "Pivotal Software, Inc." / "Neo Technology" / "Graph Aware Ltd."
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ *
+ */
+
+package org.springframework.data.neo4j.repository.query;
+
+import org.neo4j.ogm.session.Session;
+
+/**
+ * Specialisation of {@link GraphRepositoryQuery} that creates queries from named queries defined in
+ * {@code META-INFO/neo4j-named-queries.properties}.
+ *
+ * @author Gerrit Meier
+ */
+public class NamedGraphRepositoryQuery extends GraphRepositoryQuery {
+
+	private final String cypherQuery;
+
+	NamedGraphRepositoryQuery(GraphQueryMethod graphQueryMethod, Session session, String cypherQuery) {
+		super(graphQueryMethod, session);
+		this.cypherQuery = cypherQuery;
+	}
+
+	@Override
+	protected Query getQuery(Object[] parameters) {
+		return new Query(cypherQuery, resolveParams(parameters));
+	}
+
+}
