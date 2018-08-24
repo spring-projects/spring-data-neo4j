ca8feb3d6e4c9f77e6bb673153fbfdadb5b5145c
Gerrit Meier <gerrit.meier@neo4j.com>
2018-08-24 08:53:49 +0200

    DATAGRAPH-1115 - Support @UseBookmark in composed annotations.
    The annotation is not found if it is part of a composed annotation
    when using a simple Class#getAnnotation call.
    This changes the annotation access to use Springs AnnotatedElementUtils.
    
    During the testing it was noticed that tests are only found name "*Tests.java",
    so unrelated class Neo4jPersistentPropertyTest.java has been renamed
    Neo4jPersistentPropertyTests.java.
    
    # Conflicts:
    #	spring-data-neo4j/src/test/java/org/springframework/data/neo4j/mapping/Neo4jPersistentPropertyTests.java

--- spring-data-neo4j/src/test/java/org/springframework/data/neo4j/bookmark/BookmarkWrapper.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/bookmark/BookmarkWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c)  [2011-2017] "Pivotal Software, Inc." / "Neo Technology" / "Graph Aware Ltd."
+ * Copyright (c)  [2011-2018] "Pivotal Software, Inc." / "Neo Technology" / "Graph Aware Ltd."
  *
  * This product is licensed to you under the Apache License, Version 2.0 (the "License").
  * You may not use this product except in compliance with the License.
@@ -8,12 +8,12 @@
  * separate copyright notices and license terms. Your use of the source
  * code for these subcomponents is subject to the terms and
  * conditions of the subcomponent's license, as noted in the LICENSE file.
- *
  */
 
-package org.springframework.data.neo4j.annotation;
+package org.springframework.data.neo4j.bookmark;
 
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.data.neo4j.annotation.UseBookmark;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -21,22 +21,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Indicates that a transactional operation will use bookmarks that are currently stored in BookmarkManager when
- * creating Neo4j session. Must be used on a method with @Transactional annotation. May be used on class - applies to
- * all methods or on specific methods.
- * <p>
- *
- * @author Frantisek Hartman
- * @see EnableBookmarkManagement
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Documented
-public @interface UseBookmark {
+@UseBookmark
+public @interface BookmarkWrapper {
 
-	/**
-	 * If the annotated method should set bookmark
-	 */
+	@AliasFor(annotation = UseBookmark.class, attribute = "value")
 	boolean value() default true;
 }
