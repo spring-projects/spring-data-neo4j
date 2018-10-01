c5026f85f1422ea3eff66f31a73d62ce3a095a18
Michael J. Simons <michael@simons.ac>
2018-10-01 10:10:43 +0200

    DATAGRAPH-1131 - Fix possible ArrayIndexOutOfBoundsException in MetaDataDrivenConversionService during startup.
    Due to improvments in DATAGRAPH-1126 MetaDataDrivenConversionService gets actually used when there is no existing conversion services and revealed now that it can’t handle converters extending from abstract converter bases classes and not redclaring their inheritance from an interface.
    
    This commit uses now Spring Commons infrastructure method to determine generic types of a class instead of using a custom solution.

---
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/conversion/support/ConvertedClass.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c)  [2011-2018] "Pivotal Software, Inc." / "Neo Technology" / "Graph Aware Ltd."
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product may include a number of subcomponents with
+ * separate copyright notices and license terms. Your use of the source
+ * code for these subcomponents is subject to the terms and
+ * conditions of the subcomponent's license, as noted in the LICENSE file.
+ *
+ */
+
+package org.springframework.data.neo4j.conversion.support;
+
+/**
+ * @author Michael J. Simons
+ * @soundtrack Murray Gold - Doctor Who Season 9
+ */
+public class ConvertedClass {
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
