2ab69d55aa3396b8625f2cb432b717c744c79041
Michael J. Simons <michael@simons.ac>
2018-11-19 17:13:12 +0100

    DATAGRAPH-1151 - Polishing.

--- spring-data-neo4j/src/test/java/org/springframework/data/neo4j/namedquery/NamedQueryTests.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/namedquery/NamedQueryTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2017-2018 "Neo4j, Inc." / "Pivotal Software, Inc."
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.neo4j.namedquery;
 
 import static org.junit.Assert.*;
@@ -5,7 +20,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.neo4j.ogm.session.SessionFactory;
-import org.neo4j.ogm.testutil.MultiDriverTestClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,10 +34,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @author Gerrit Meier
+ * @author Michael J. Simons
+ */
 @ContextConfiguration(classes = { NamedQueryTests.NamedQueryContext.class })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
-public class NamedQueryTests extends MultiDriverTestClass {
+public class NamedQueryTests {
 
 	private static final String SAMPLE_ENTITY_NAME = "test";
 
