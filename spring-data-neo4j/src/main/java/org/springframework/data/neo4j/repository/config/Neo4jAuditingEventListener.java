24b8fbab32ef506c26510dd7cc89d46b32e3fb4a
Mark Paluch <mpaluch@pivotal.io>
2019-01-08 08:54:07 +0100

    DATAGRAPH-1173 - Update copyright years to 2019.

--- spring-data-neo4j/src/main/java/org/springframework/data/neo4j/repository/config/Neo4jAuditingEventListener.java
+++ spring-data-neo4j/src/main/java/org/springframework/data/neo4j/repository/config/Neo4jAuditingEventListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2017 "Neo Technology,"
+ * Copyright (c) 2002-2019 "Neo Technology,"
  * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This product is licensed to you under the Apache License, Version 2.0 (the "License").
--- spring-data-neo4j/src/test/java/org/springframework/data/neo4j/namedquery/NamedQueryTests.java
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/namedquery/NamedQueryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018 "Neo4j, Inc." / "Pivotal Software, Inc."
+ * Copyright (c) 2017-2019 "Neo4j, Inc." / "Pivotal Software, Inc."
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
