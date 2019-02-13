cdc86ad1e412616869b1e721c473fe1c93cedfc1
Gerrit Meier <meistermeier@gmail.com>
2019-02-13 20:35:44 +0100

    Add EnableNeo4jRepositories annotation.
    This is mostly a stub because it does nothing yet besides
    providing the basic configuration attributes for package scanning.

---
+++ spring-data-neo4j-rx/src/main/java/org/springframework/data/neo4j/repository/config/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Configuration infrastructure for Neo4j specific repositories.
+ */
+@NonNullApi
+package org.springframework.data.neo4j.repository.config;
+
+import org.springframework.lang.NonNullApi;
