c8327079ed8b5f94d647a0542659fa87e96914df
Michael Simons <michael.simons@neo4j.com>
2019-02-13 13:04:04 +0100

    Add example module.

---
+++ spring-data-neo4j-rx-examples/src/main/java/org/springframework/data/neo4j/examples/references_and_aggregates/books/PurchaseOrder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.examples.references_and_aggregates.books;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * @author Michael J. Simons
+ */
+public class PurchaseOrder {
+	@Id
+	private Long id;
+	private final String shippingAddress;
+	private final Set<OrderItem> items = new HashSet<>();
+
+	public PurchaseOrder(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+	PurchaseOrder addItem(String product, int quantity) {
+
+		items.add(createOrderItem(product, quantity));
+		return this;
+	}
+
+	private static OrderItem createOrderItem(String product, int quantity) {
+
+		OrderItem item = new OrderItem(product, quantity);
+		return item;
+	}
+}
---
+++ spring-data-neo4j-rx-examples/src/main/java/org/springframework/data/neo4j/examples/references_and_aggregates/books/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * See: <a href="https://spring.io/blog/2018/09/24/spring-data-jdbc-references-and-aggregates">Spring Data JDBC, References, and Aggregates</a>.
+ *
+ * @author Michael J. Simons
+ */
+package org.springframework.data.neo4j.examples.references_and_aggregates.books;
