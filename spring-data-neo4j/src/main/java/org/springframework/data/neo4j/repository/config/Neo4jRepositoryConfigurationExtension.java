25101eb1793b47ecee690716f65d37b84f338d9e
Michael Simons <michael.simons@neo4j.com>
2019-02-14 10:23:29 +0100

    Add repository configuration and factory beans.

---
+++ spring-data-neo4j/src/main/java/org/springframework/data/neo4j/repository/config/Neo4jRepositoryConfigurationExtension.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.repository.config;
+
+import org.springframework.data.neo4j.repository.support.Neo4jRepositoryFactoryBean;
+import org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport;
+
+/**
+ * This dedicated Neo4j repository extension will be registered via {@link Neo4jRepositoriesRegistrar} and then provide
+ * all necessary beans to be registered in the application's context before the user's "business" beans gets registered.
+ *
+ * @author Michael J. Simons
+ * @author Gerrit Meier
+ */
+class Neo4jRepositoryConfigurationExtension extends RepositoryConfigurationExtensionSupport {
+
+	private static final String MODULE_PREFIX = "neo4j";
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.config14.RepositoryConfigurationExtension#getRepositoryFactoryBeanClassName()
+	 */
+	@Override
+	public String getRepositoryFactoryBeanClassName() {
+		return Neo4jRepositoryFactoryBean.class.getName();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.config14.RepositoryConfigurationExtensionSupport#getModulePrefix()
+	 */
+	@Override
+	protected String getModulePrefix() {
+		return MODULE_PREFIX;
+	}
+}
