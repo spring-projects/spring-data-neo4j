9846a46abc3545c01a8a03189d74f3e625198396
Michael Simons <michael.simons@neo4j.com>
2019-02-18 12:40:51 +0100

    Add suggestion for @Query-infrastructure.

---
+++ spring-data-neo4j/src/main/java/org/springframework/data/neo4j/repository/query/ExistsQuery.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.repository.query;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specialized version of {@link Query} whose values is always used as exists projection.
+ *
+ * @author Michael J. Simons
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+@Documented
+@Query(exists = true)
+public @interface ExistsQuery {
+}
---
+++ spring-data-neo4j/src/main/java/org/springframework/data/neo4j/repository/query/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A set of annotations for providing custom queries to repositories.
+ */
+@NonNullApi
+package org.springframework.data.neo4j.repository.query;
+
+import org.springframework.lang.NonNullApi;
