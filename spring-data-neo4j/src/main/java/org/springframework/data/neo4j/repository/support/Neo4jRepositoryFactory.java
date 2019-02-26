6659f8fc2621df57df87599fe4699c20f8417a07
Gerrit Meier <meistermeier@gmail.com>
2019-02-26 14:01:04 +0100

    Infrastructure to enable Neo4jRepositories.
    This will introduce the infrastructure needed to initialize
    the basic `SimpleNeo4jRepository` and autowire a given `Neo4jTemplate`
    to it when `@EnableNeo4jRepositories` is set.

---
+++ spring-data-neo4j/src/main/java/org/springframework/data/neo4j/repository/support/Neo4jRepositoryFactory.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.repository.support;
+
+import org.springframework.data.neo4j.core.Neo4jOperations;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.core.EntityInformation;
+import org.springframework.data.repository.core.RepositoryInformation;
+import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+
+/**
+ * Factory to create {@link Neo4jRepository} instances.
+ *
+ * @author Gerrit Meier
+ */
+public class Neo4jRepositoryFactory extends RepositoryFactorySupport {
+
+	private final Neo4jOperations neo4jOperations;
+
+	public Neo4jRepositoryFactory(Neo4jOperations neo4jOperations) {
+		this.neo4jOperations = neo4jOperations;
+	}
+
+	@Override
+	public <T, ID> EntityInformation<T, ID> getEntityInformation(Class<T> domainClass) {
+		return null;
+	}
+
+	@Override
+	protected Object getTargetRepository(RepositoryInformation metadata) {
+		return getTargetRepositoryViaReflection(metadata, neo4jOperations);
+	}
+
+	@Override
+	protected Class<?> getRepositoryBaseClass(RepositoryMetadata metadata) {
+		return SimpleNeo4jRepository.class;
+	}
+}
