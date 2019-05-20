623624ef836269b6014658bf26be8c84177718b3
Michael Simons <michael.simons@neo4j.com>
2019-05-20 23:01:59 +0200

    Add support for parameters in String based queries.

--- spring-data-neo4j-rx/src/main/java/org/springframework/data/neo4j/repository/query/PartTreeNeo4jQuery.java
+++ spring-data-neo4j-rx/src/main/java/org/springframework/data/neo4j/repository/query/PartTreeNeo4jQuery.java
@@ -30,16 +30,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.neo4j.driver.types.Point;
-import org.springframework.data.domain.Range;
-import org.springframework.data.geo.Circle;
-import org.springframework.data.geo.Distance;
-import org.springframework.data.geo.Metrics;
 import org.springframework.data.neo4j.core.NodeManager;
 import org.springframework.data.neo4j.core.PreparedQuery;
 import org.springframework.data.neo4j.core.mapping.Neo4jMappingContext;
@@ -64,7 +59,6 @@ import org.springframework.util.Assert;
 @Slf4j
 final class PartTreeNeo4jQuery extends AbstractNeo4jQuery {
 
-	//
 	/**
 	 * A set of the temporal types that are directly passable to the driver and support a meaningful comparision in a
 	 * temporal sense (after, before).
@@ -93,7 +87,8 @@ final class PartTreeNeo4jQuery extends AbstractNeo4jQuery {
 		this.tree = new PartTree(queryMethod.getName(), domainType);
 
 		// Validate parts. Sort properties will be validated by Spring Data already.
-		this.tree.flatMap(OrPart::stream).forEach(part -> validatePart(part));
+		PartValidator validator = new PartValidator(queryMethod);
+		this.tree.flatMap(OrPart::stream).forEach(validator::validatePart);
 	}
 
 	@Override
@@ -118,104 +113,16 @@ final class PartTreeNeo4jQuery extends AbstractNeo4jQuery {
 			.build();
 	}
 
-	void validatePart(Part part) {
-
-		validateIgnoreCase(part);
-		switch (part.getType()) {
-			case AFTER:
-			case BEFORE:
-				validateTemporalProperty(part);
-				break;
-			case IS_EMPTY:
-			case IS_NOT_EMPTY:
-				validateCollectionProperty(part);
-				break;
-			case NEAR:
-			case WITHIN:
-				validatePointProperty(part);
-				break;
-		}
-	}
-
+	/**
+	 * Checks whether the given part can be queried without case sensitivity.
+	 *
+	 * @param part
+	 * @return True when {@code part} can be queried case insensitive.
+	 */
 	static boolean canIgnoreCase(Part part) {
 		return part.getProperty().getLeafType() == String.class && TYPES_SUPPORTING_CASE_INSENSITIVITY
 			.contains(part.getType());
 	}
-
-	private String formatTypes(Collection<Part.Type> types) {
-		return types.stream().flatMap(t -> t.getKeywords().stream()).collect(joining(", ", "[", "]"));
-	}
-
-	void validateIgnoreCase(Part part) {
-
-		Assert.state(part.shouldIgnoreCase() != Part.IgnoreCaseType.ALWAYS || canIgnoreCase(part),
-			() -> String.format(
-				"Can not derive query for '%s': Only the case of String based properties can be ignored within the following keywords: %s",
-				super.queryMethod,
-				formatTypes(TYPES_SUPPORTING_CASE_INSENSITIVITY)));
-	}
-
-	void validateTemporalProperty(Part part) {
-
-		Assert.state(COMPARABLE_TEMPORAL_TYPES.contains(part.getProperty().getLeafType()), () -> String
-			.format(
-				"Can not derive query for '%s': The keywords %s work only with properties with one of the following types: %s",
-				super.queryMethod, formatTypes(Collections.singletonList(part.getType())),
-				COMPARABLE_TEMPORAL_TYPES));
-	}
-
-	void validateCollectionProperty(Part part) {
-		Assert.state(part.getProperty().getLeafProperty().isCollection(), () -> String
-			.format("Can not derive query for '%s': The keywords %s work only with collection properties",
-				super.queryMethod,
-				formatTypes(Collections.singletonList(part.getType()))));
-	}
-
-	void validatePointProperty(Part part) {
-
-		Assert.state(ClassTypeInformation.from(Point.class)
-			.isAssignableFrom(part.getProperty().getLeafProperty().getTypeInformation()), () -> String
-			.format("Can not derive query for '%s': %s works only with spatial properties", super.queryMethod,
-				part.getType()));
-	}
-
-	/**
-	 * Converts parameter as needed by the query generated, which is not covered by standard conversion services
-	 *
-	 * @param parameter The parameter to fit into the generated query.
-	 * @return A parameter that fits the place holders of a generated query
-	 */
-	static Object convertParameter(Object parameter) {
-		if (parameter instanceof Range) {
-			Range range = (Range) parameter;
-			Map<String, Object> map = new HashMap<>();
-			range.getLowerBound().getValue().map(PartTreeNeo4jQuery::convertParameter).ifPresent(v -> map.put("lb", v));
-			range.getUpperBound().getValue().map(PartTreeNeo4jQuery::convertParameter).ifPresent(v -> map.put("ub", v));
-			return map;
-		} else if (parameter instanceof Distance) {
-			return calculateDistanceInMeter((Distance) parameter);
-		} else if (parameter instanceof Circle) {
-			Circle circle = (Circle) parameter;
-			Map<String, Object> map = new HashMap<>();
-			map.put("x", convertParameter(circle.getCenter().getX()));
-			map.put("y", convertParameter(circle.getCenter().getY()));
-			map.put("radius", convertParameter(calculateDistanceInMeter(circle.getRadius())));
-			return map;
-		}
-		return parameter;
-	}
-
-	private static double calculateDistanceInMeter(Distance distance) {
-
-		if (distance.getMetric() == Metrics.KILOMETERS) {
-			return distance.getValue() / 0.001d;
-		} else if (distance.getMetric() == Metrics.MILES) {
-			return distance.getValue() / 0.00062137d;
-		} else {
-			return distance.getValue();
-		}
-	}
-
 
 	@Override
 	protected boolean isCountQuery() {
@@ -235,5 +142,72 @@ final class PartTreeNeo4jQuery extends AbstractNeo4jQuery {
 	@Override
 	protected boolean isLimiting() {
 		return tree.isLimiting();
+	}
+
+	static class PartValidator {
+
+		private final Neo4jQueryMethod queryMethod;
+
+		PartValidator(Neo4jQueryMethod queryMethod) {
+			this.queryMethod = queryMethod;
+		}
+
+		void validatePart(Part part) {
+
+			validateIgnoreCase(part);
+			switch (part.getType()) {
+				case AFTER:
+				case BEFORE:
+					validateTemporalProperty(part);
+					break;
+				case IS_EMPTY:
+				case IS_NOT_EMPTY:
+					validateCollectionProperty(part);
+					break;
+				case NEAR:
+				case WITHIN:
+					validatePointProperty(part);
+					break;
+			}
+		}
+
+		private void validateIgnoreCase(Part part) {
+
+			Assert.state(part.shouldIgnoreCase() != Part.IgnoreCaseType.ALWAYS || canIgnoreCase(part),
+				() -> String.format(
+					"Can not derive query for '%s': Only the case of String based properties can be ignored within the following keywords: %s",
+					queryMethod,
+					formatTypes(TYPES_SUPPORTING_CASE_INSENSITIVITY)));
+		}
+
+		private void validateTemporalProperty(Part part) {
+
+			Assert.state(COMPARABLE_TEMPORAL_TYPES.contains(part.getProperty().getLeafType()), () -> String
+				.format(
+					"Can not derive query for '%s': The keywords %s work only with properties with one of the following types: %s",
+					queryMethod, formatTypes(Collections.singletonList(part.getType())),
+					COMPARABLE_TEMPORAL_TYPES));
+		}
+
+		private void validateCollectionProperty(Part part) {
+
+			Assert.state(part.getProperty().getLeafProperty().isCollection(), () -> String
+				.format("Can not derive query for '%s': The keywords %s work only with collection properties",
+					queryMethod,
+					formatTypes(Collections.singletonList(part.getType()))));
+		}
+
+		private void validatePointProperty(Part part) {
+
+			Assert.state(ClassTypeInformation.from(Point.class)
+				.isAssignableFrom(part.getProperty().getLeafProperty().getTypeInformation()), () -> String
+				.format("Can not derive query for '%s': %s works only with spatial properties", queryMethod,
+					part.getType()));
+		}
+
+		private static String formatTypes(Collection<Part.Type> types) {
+			return types.stream().flatMap(t -> t.getKeywords().stream()).collect(joining(", ", "[", "]"));
+		}
+
 	}
 }
--- spring-data-neo4j-rx/src/main/java/org/springframework/data/neo4j/repository/query/StringBasedNeo4jQuery.java
+++ spring-data-neo4j-rx/src/main/java/org/springframework/data/neo4j/repository/query/StringBasedNeo4jQuery.java
@@ -18,16 +18,36 @@
  */
 package org.springframework.data.neo4j.repository.query;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.data.mapping.MappingException;
 import org.springframework.data.neo4j.core.NodeManager;
 import org.springframework.data.neo4j.core.PreparedQuery;
 import org.springframework.data.neo4j.core.mapping.Neo4jMappingContext;
+import org.springframework.data.repository.query.Parameter;
+import org.springframework.data.repository.query.Parameters;
+import org.springframework.data.repository.query.QueryMethodEvaluationContextProvider;
 import org.springframework.data.repository.query.RepositoryQuery;
+import org.springframework.data.repository.query.SpelEvaluator;
+import org.springframework.data.repository.query.SpelQueryContext;
+import org.springframework.data.repository.query.SpelQueryContext.SpelExtractor;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 /**
- * Implementation of {@link RepositoryQuery} for String based custom Cypher query.
+ * Implementation of {@link RepositoryQuery} for query methods annotated with {@link Query @Query}.
+ *
+ *
+ * The flow to handle queries with SpEL parameters is as follows
+ * <ol>
+ * <li>Parse template as something that has SpEL-expressions in it</li>
+ * <li>Replace the SpEL-expressions with Neo4j Statement template parameters</li>
+ * <li>The parameters passed here _and_ the values that might have been computed during SpEL-parsing</li>
+ * </ol>
+ * The main ingredient is a SpelEvaluator, that parses a template and replaces SpEL expressions
+ * with real Neo4j parameters.
  *
  * @author Gerrit Meier
  * @author Michael J. Simons
@@ -35,30 +55,105 @@ import org.springframework.data.repository.query.RepositoryQuery;
  */
 final class StringBasedNeo4jQuery extends AbstractNeo4jQuery {
 
-	private final String cypherQuery;
+	/**
+	 * Used for extracting SpEL expressions inside Cypher query templates.
+	 */
+	static final SpelQueryContext SPEL_QUERY_CONTEXT = SpelQueryContext
+		.of(StringBasedNeo4jQuery::parameterNameSource, StringBasedNeo4jQuery::replacementSource);
 
+	/**
+	 * Is this a count projection?
+	 */
 	private final boolean countQuery;
+
+	/**
+	 * Is this an exists projection?
+	 */
 	private final boolean existsQuery;
+
+	/**
+	 * Is this a modifying delete query?
+	 */
 	private final boolean deleteQuery;
 
-	StringBasedNeo4jQuery(NodeManager nodeManager, Neo4jMappingContext mappingContext, Neo4jQueryMethod queryMethod,
-		String cypherQuery,
-		Optional<Query> optionalQueryAnnotation) {
+	/**
+	 * Used to evaluate the expression found while parsing the cypher template of this query against the actual parameters
+	 * with the help of the formal parameters during the building of the {@link PreparedQuery}.
+	 */
+	private final SpelEvaluator spelEvaluator;
+
+	/**
+	 * The Cypher string used for this query. The cypher query will not be changed after parsed via {@link #SPEL_QUERY_CONTEXT}.
+	 * All SpEL expressions will be substituted via "native" parameter placeholders. This will be done via the {@link #spelEvaluator}.
+	 */
+	private final String cypherQuery;
+
+	/**
+	 * Create a {@link StringBasedNeo4jQuery} for a query method that is annotated with {@link Query @Query}. The annotation
+	 * is expected to have a value.
+	 *
+	 * @param nodeManager
+	 * @param mappingContext
+	 * @param evaluationContextProvider
+	 * @param queryMethod
+	 * @return A new instance of a String based Neo4j query.
+	 */
+	static StringBasedNeo4jQuery create(NodeManager nodeManager, Neo4jMappingContext mappingContext,
+		QueryMethodEvaluationContextProvider evaluationContextProvider,
+		Neo4jQueryMethod queryMethod) {
+
+		Query queryAnnotation = queryMethod.getQueryAnnotation()
+			.orElseThrow(() -> new MappingException("Expected @Query annotation on the query method!"));
+
+		String cypherTemplate = Optional.ofNullable(queryAnnotation.value())
+			.filter(StringUtils::hasText)
+			.orElseThrow(() -> new MappingException("Expected @Query annotation to have a value, but it did not."));
+
+		return new StringBasedNeo4jQuery(nodeManager, mappingContext, evaluationContextProvider, queryMethod,
+			cypherTemplate, queryAnnotation.count(), queryAnnotation.exists(), queryAnnotation.delete());
+	}
+
+	/**
+	 * Create a {@link StringBasedNeo4jQuery} based on an explicit Cypher template.
+	 *
+	 * @param nodeManager
+	 * @param mappingContext
+	 * @param evaluationContextProvider
+	 * @param queryMethod
+	 * @param cypherTemplate            The template to use.
+	 * @return A new instance of a String based Neo4j query.
+	 */
+	static StringBasedNeo4jQuery create(NodeManager nodeManager, Neo4jMappingContext mappingContext,
+		QueryMethodEvaluationContextProvider evaluationContextProvider,
+		Neo4jQueryMethod queryMethod, String cypherTemplate) {
+
+		Assert.hasText(cypherTemplate, "Cannot create String based Neo4j query without a cypher template.");
+
+		return new StringBasedNeo4jQuery(nodeManager, mappingContext, evaluationContextProvider, queryMethod,
+			cypherTemplate, false, false, false);
+	}
+
+	private StringBasedNeo4jQuery(NodeManager nodeManager,
+		Neo4jMappingContext mappingContext, QueryMethodEvaluationContextProvider evaluationContextProvider,
+		Neo4jQueryMethod queryMethod, String cypherTemplate, boolean countQuery,
+		boolean existsQuery, boolean deleteQuery) {
 
 		super(nodeManager, mappingContext, queryMethod);
 
-		this.cypherQuery = cypherQuery;
+		this.countQuery = countQuery;
+		this.existsQuery = existsQuery;
+		this.deleteQuery = deleteQuery;
 
-		if (optionalQueryAnnotation.isPresent()) {
-			Query queryAnnotation = optionalQueryAnnotation.get();
-			countQuery = queryAnnotation.count();
-			existsQuery = queryAnnotation.exists();
-			deleteQuery = queryAnnotation.delete();
-		} else {
-			countQuery = false;
-			existsQuery = false;
-			deleteQuery = false;
-		}
+		SpelExtractor spelExtractor = SPEL_QUERY_CONTEXT.parse(cypherTemplate);
+		this.spelEvaluator = new SpelEvaluator(evaluationContextProvider, queryMethod.getParameters(), spelExtractor);
+		this.cypherQuery = spelExtractor.getQueryString();
+	}
+
+	static String getQueryTemplate(Query queryAnnotation) {
+
+		return Optional.ofNullable(queryAnnotation.value())
+			.filter(StringUtils::hasText)
+			.orElseThrow(() -> new MappingException("Expected @Query annotation to have a value, but it did not."));
 	}
 
 	@Override
@@ -66,7 +161,7 @@ final class StringBasedNeo4jQuery extends AbstractNeo4jQuery {
 
 		return PreparedQuery.queryFor(super.domainType)
 			.withCypherQuery(cypherQuery)
-			.withParameters(Collections.emptyMap()) // TODO Map parameters.
+			.withParameters(bindParameters(parameters))
 			.usingMappingFunction(mappingContext.getMappingFunctionFor(super.domainType).orElse(null)) // Null is fine
 			.build();
 	}
@@ -89,5 +184,46 @@ final class StringBasedNeo4jQuery extends AbstractNeo4jQuery {
 	@Override
 	protected boolean isLimiting() {
 		return false;
+	}
+
+	Map<String, Object> bindParameters(Object[] actualParameters) {
+
+		final Parameters<?, ?> formalParameters = queryMethod.getParameters();
+
+		Map<String, Object> resolvedParameters = new HashMap<>(spelEvaluator.evaluate(actualParameters));
+		formalParameters.stream()
+			.filter(Parameter::isBindable)
+			.forEach(parameter -> {
+
+				int parameterIndex = parameter.getIndex();
+				Object parameterValue = super.convertParameter(actualParameters[parameterIndex]);
+
+				// Add the parameter under it's name when possible
+				parameter.getName()
+					.ifPresent(parameterName -> resolvedParameters.put(parameterName, parameterValue));
+				// Always add under its index.
+				resolvedParameters.put(Integer.toString(parameterIndex), parameterValue);
+			});
+
+		return resolvedParameters;
+	}
+
+	/**
+	 * @param index
+	 * @param originalSpelExpression Not used for configuring parameter names atm.
+	 * @return
+	 */
+	private static String parameterNameSource(int index, @SuppressWarnings("unused") String originalSpelExpression) {
+		return "__SpEL__" + index;
+	}
+
+	/**
+	 * @param originalPrefix The prefix passed to the replacement source is either ':' or '?', so that isn't usable for
+	 *                       Cypher templates and therefore ignored.
+	 * @param parameterName
+	 * @return
+	 */
+	private static String replacementSource(@SuppressWarnings("unused") String originalPrefix, String parameterName) {
+		return "$" + parameterName;
 	}
 }
