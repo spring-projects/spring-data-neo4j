cd7864cca1eeb455bc5992f3ab53729dd9797549
Gerrit Meier <meistermeier@gmail.com>
2019-06-04 18:20:19 +0200

    Remove unneeded import from test.

--- spring-data-neo4j-rx/src/test/java/org/neo4j/springframework/data/core/TransactionHandlingTest.java
+++ spring-data-neo4j-rx/src/test/java/org/neo4j/springframework/data/core/TransactionHandlingTest.java
@@ -29,7 +29,6 @@ import java.util.function.Consumer;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
