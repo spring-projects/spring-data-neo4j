fe6b177fdc4115e991f80fabba76ce3c5828224b
Gerrit Meier <meistermeier@gmail.com>
2019-06-12 10:54:27 +0200

    Add missing package-info files.

---
+++ spring-data-neo4j-rx/src/main/java/org/neo4j/springframework/data/core/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the core infrastructure for creating a imperative or reactive client that can execute
+ * queries.
+ */
+@NonNullApi
+package org.neo4j.springframework.data.core;
+
+import org.springframework.lang.NonNullApi;
