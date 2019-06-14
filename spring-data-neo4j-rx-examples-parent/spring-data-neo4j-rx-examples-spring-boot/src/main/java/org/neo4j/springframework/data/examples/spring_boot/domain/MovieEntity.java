b970b030ada25c3a962fa5e6cb12c5d58474f9ea
Michael Simons <michael.simons@neo4j.com>
2019-06-14 16:40:02 +0200

    Prepare Spring Boot demo.

---
+++ spring-data-neo4j-rx-examples-parent/spring-data-neo4j-rx-examples-spring-boot/src/main/java/org/neo4j/springframework/data/examples/spring_boot/domain/MovieEntity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.springframework.data.examples.spring_boot.domain;
+
+import org.neo4j.springframework.data.core.schema.Id;
+import org.neo4j.springframework.data.core.schema.Node;
+import org.neo4j.springframework.data.core.schema.Property;
+
+/**
+ * @author Michael J. Simons
+ */
+@Node("Movie")
+public class MovieEntity {
+
+	@Id
+	private final Long id;
+
+	private final String title;
+
+	@Property("tagline")
+	private final String description;
+
+	MovieEntity(Long id, String title, String description) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public MovieEntity withId(Long id) {
+		return this.id == id ?
+			this : new MovieEntity(id, this.title, this.description);
+	}
+}
