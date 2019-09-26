4a186f9dfcb277f286ede1b583d54622b0b52bd2
Gerrit Meier <meistermeier@gmail.com>
2019-09-26 11:55:33 +0200

    Add Kotlin section to .editorconfig.

--- spring-data-neo4j-rx/src/main/kotlin/org/neo4j/springframework/data/core/PreparedQueryFactory.kt
+++ spring-data-neo4j-rx/src/main/kotlin/org/neo4j/springframework/data/core/PreparedQueryFactory.kt
@@ -27,6 +27,6 @@ import kotlin.reflect.KClass
  * @since 1.0
  */
 class PreparedQueryFactory<T : Any>(val c: KClass<T>) {
-	fun withCypherQuery(cypherQuery: String): PreparedQuery.OptionalBuildSteps<T> = c.javaObjectType.let { PreparedQuery.queryFor(it) }
-			.withCypherQuery(cypherQuery)
+    fun withCypherQuery(cypherQuery: String): PreparedQuery.OptionalBuildSteps<T> = c.javaObjectType.let { PreparedQuery.queryFor(it) }
+            .withCypherQuery(cypherQuery)
 }
