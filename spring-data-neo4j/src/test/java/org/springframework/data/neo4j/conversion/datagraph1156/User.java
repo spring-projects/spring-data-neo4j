ebdf0f18a3cce19e3223ba243f5c6d6af7cf7158
Michael Simons <michael@simons.ac>
2020-08-27 09:34:48 +0200

    DATAGRAPH-1156 - Verify custom ID types for related nodes.

---
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/conversion/datagraph1156/User.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.conversion.datagraph1156;
+
+import java.util.UUID;
+
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+import org.neo4j.ogm.annotation.typeconversion.Convert;
+import org.neo4j.ogm.typeconversion.UuidStringConverter;
+
+/**
+ * @author Michael J. Simons
+ */
+@NodeEntity
+public class User {
+
+	@Id
+	@Property("id")
+	@Convert(value = UuidStringConverter.class)
+	private UUID id = UUID.randomUUID();
+
+	@Property("email")
+	private String email;
+
+	public User(String email) {
+		this.email = email;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+}
