952b1bfa2f2fdbb910b1e0e57cfc07757f41f996
Michael Simons <michael@simons.ac>
2020-08-28 12:17:38 +0200

    DATAGRAPH-1303 - Ensure that unrelated annotation don’t break OGM metadata.
    This commits adds two tests ensuring that OGM 3.2.15 and 3.1.21 don’t fail on unrelated, package private annotations any more.

---
+++ spring-data-neo4j/src/test/java/org/springframework/data/neo4j/mapping/AnnotationScanningTests.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2011-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.mapping;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.neo4j.ogm.metadata.AnnotationsInfo;
+import org.neo4j.ogm.session.SessionFactory;
+import org.neo4j.ogm.testutil.MultiDriverTestClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Michael J. Simons
+ * @soundtrack Metallica - S&M2
+ */
+@ContextConfiguration(classes = { AnnotationScanningTests.Config.class })
+@RunWith(SpringRunner.class)
+public class AnnotationScanningTests extends MultiDriverTestClass {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Test
+	public void shouldBeAbleToWorkWithAnnotatedAnnotations() {
+		AnnotationsInfo annotations = sessionFactory.metaData().classInfo("TheEndpointController").annotationsInfo();
+		assertThat(annotations.get("org.springframework.data.neo4j.mapping.datagraph1303.EndpointController"))
+				.isNotNull().extracting(i -> i.get("value"))
+				.isEqualTo("foobar");
+	}
+
+	@Configuration
+	static class Config {
+
+		@Bean
+		public SessionFactory sessionFactory() {
+			return new SessionFactory(getBaseConfiguration().build(),
+					"org.springframework.data.neo4j.mapping.datagraph1303");
+		}
+	}
+}
