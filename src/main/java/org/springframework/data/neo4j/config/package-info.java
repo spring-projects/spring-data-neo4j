4b3df4087f1a8fe031b421ff43002842feb4f286
Florent Biville <445792+fbiville@users.noreply.github.com>
2020-11-04 09:43:14 +0100

    DATAGRAPH-1421 - Add another round of copy editing.
    Co-authored-by: Michael Simons <michael@simons.ac>

--- src/main/java/org/springframework/data/neo4j/config/package-info.java
+++ src/main/java/org/springframework/data/neo4j/config/package-info.java
@@ -1,7 +1,7 @@
 /**
  * <!-- tag::intent[] -->
    This package contains configuration related support classes that can be used for application specific, annotated
-   configuration classes. The abstract base classes are helpful if you don't rely on Spring Boots autoconfiguration.
+   configuration classes. The abstract base classes are helpful if you don't rely on Spring Boot's autoconfiguration.
    The package provides some additional annotations that enable auditing.
  * <!-- end::intent[] -->
  */
