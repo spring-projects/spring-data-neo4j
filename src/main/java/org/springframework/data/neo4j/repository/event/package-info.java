b94253a952429731baa22c325e2628d8eb762658
Michael Simons <michael@simons.ac>
2020-11-24 21:13:11 +0100

    Make IntelliJ not barf on deprecated packages…
    While Java allows this, IntelliJ won’t… Well, so be it.

--- src/main/java/org/springframework/data/neo4j/repository/event/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This package has been deprecated. Please use the infrastructure in {@literal org.springframework.data.neo4j.core.mapping.callback}.
- * @deprecated since 6.0.2
- */
-@NonNullApi
-@Deprecated
-package org.springframework.data.neo4j.repository.event;
-
-import org.springframework.lang.NonNullApi;
