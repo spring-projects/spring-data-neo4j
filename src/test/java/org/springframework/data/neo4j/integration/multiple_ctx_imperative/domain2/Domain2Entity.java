3180bc5b2cb5f242d214f642c544f6e650fda3df
Michael Simons <michael@simons.ac>
2020-11-24 17:35:23 +0100

    Add tests for multipel contexts (not yet completed).

---
+++ src/test/java/org/springframework/data/neo4j/integration/multiple_ctx_imperative/domain2/Domain2Entity.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2011-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.integration.multiple_ctx_imperative.domain2;
+
+import org.springframework.data.annotation.Version;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+/**
+ * @author Michael J. Simons
+ * @soundtrack Kelis - Tasty
+ */
+@Node
+public class Domain2Entity {
+
+	@Id @GeneratedValue
+	private Long id;
+
+	@Version
+	private Long version;
+
+	private String anAttribute;
+
+	public Domain2Entity(String anAttribute) {
+		this.anAttribute = anAttribute;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public String getAnAttribute() {
+		return anAttribute;
+	}
+
+	public void setAnAttribute(String anAttribute) {
+		this.anAttribute = anAttribute;
+	}
+}
