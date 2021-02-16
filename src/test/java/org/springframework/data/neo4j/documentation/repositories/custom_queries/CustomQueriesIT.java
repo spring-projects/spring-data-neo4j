54294e06aa0672934cf901fb462fe616e83db9f9
Michael Simons <michael@simons.ac>
2021-02-16 11:31:32 +0100

    GH-2147 - Upgrade test dependencies.
    During the test two issues in the docs have been found and fixed and one
    unused variable was noticed and removed in a test as well.
    
    This closes #2147.

--- src/test/java/org/springframework/data/neo4j/documentation/repositories/custom_queries/CustomQueriesIT.java
+++ src/test/java/org/springframework/data/neo4j/documentation/repositories/custom_queries/CustomQueriesIT.java
@@ -17,9 +17,7 @@ package org.springframework.data.neo4j.documentation.repositories.custom_queries
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -76,9 +74,7 @@ class CustomQueriesIT {
 	@BeforeAll
 	static void setupData(@Autowired Driver driver) throws IOException {
 
-		try (BufferedReader moviesReader = new BufferedReader(
-				new InputStreamReader(CustomQueriesIT.class.getResourceAsStream("/data/movies.cypher")));
-				Session session = driver.session()) {
+		try (Session session = driver.session()) {
 			session.run("MATCH (n) DETACH DELETE n").consume();
 			session.run("MATCH (n) DETACH DELETE n").consume();
 			CypherUtils.loadCypherFromResource("/data/movies.cypher", session);
