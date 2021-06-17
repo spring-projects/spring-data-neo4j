e8c21611e64fb972d9af86c913ce5f7f27d1e77c
Michael Simons <michael@simons.ac>
2021-06-17 12:58:15 +0200

    GH-2289 - Don’t use the fromId with obverse to stop traversing relationships.
    The previous check used the obverse combined with the fromId to check if traversal can be stopped or not. This is wrong, we would need to combine obverse with targetId, which we may not have at the moment.
    
    This change simplifies the check and only looks at visited relationships from source to somewhere else to avoid additional rounds.
    
    This fixes #2289.

---
+++ src/test/java/org/springframework/data/neo4j/integration/issues/gh2289/ReactiveGH2289IT.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2011-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.integration.issues.gh2289;
+
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.AbstractReactiveNeo4jConfig;
+import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
+import org.springframework.data.neo4j.repository.config.EnableReactiveNeo4jRepositories;
+import org.springframework.data.neo4j.test.BookmarkCapture;
+import org.springframework.data.neo4j.test.Neo4jExtension;
+import org.springframework.data.neo4j.test.Neo4jIntegrationTest;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Michael J. Simons
+ */
+@Neo4jIntegrationTest
+@Tag(Neo4jExtension.NEEDS_REACTIVE_SUPPORT)
+class ReactiveGH2289IT {
+
+	protected static Neo4jExtension.Neo4jConnectionSupport neo4jConnectionSupport;
+
+	@BeforeAll
+	protected static void setupData(@Autowired BookmarkCapture bookmarkCapture) {
+		try (Session session = neo4jConnectionSupport.getDriver().session(bookmarkCapture.createSessionConfig());
+				Transaction transaction = session.beginTransaction();
+		) {
+			transaction.run("MATCH (n) detach delete n");
+			transaction.commit();
+			bookmarkCapture.seedWith(session.lastBookmark());
+		}
+	}
+
+	@RepeatedTest(23)
+	void testNewRelation(@Autowired SkuRepository skuRepo) {
+
+		AtomicLong bId = new AtomicLong();
+		AtomicReference<Sku> cRef = new AtomicReference<>();
+		skuRepo.save(new Sku(0L, "A"))
+				.zipWith(skuRepo.save(new Sku(1L, "B")))
+				.zipWith(skuRepo.save(new Sku(2L, "C")))
+				.zipWith(skuRepo.save(new Sku(3L, "D"))).flatMap(t -> {
+			Sku a = t.getT1().getT1().getT1();
+			Sku b = t.getT1().getT1().getT2();
+			Sku c = t.getT1().getT2();
+			Sku d = t.getT2();
+
+			bId.set(b.getId());
+			cRef.set(c);
+			a.rangeRelationTo(b, 1, 1, RelationType.MULTIPLICATIVE);
+			a.rangeRelationTo(c, 1, 1, RelationType.MULTIPLICATIVE);
+			a.rangeRelationTo(d, 1, 1, RelationType.MULTIPLICATIVE);
+			return skuRepo.save(a);
+		}).as(StepVerifier::create)
+				.expectNextMatches(a -> a.getRangeRelationsOut().size() == 3)
+				.verifyComplete();
+
+		skuRepo.findById(bId.get())
+				.doOnNext(b -> Assertions.assertThat(b.getRangeRelationsIn()).hasSize(1))
+				.flatMap(b -> {
+					b.rangeRelationTo(cRef.get(), 1, 1, RelationType.MULTIPLICATIVE);
+					return skuRepo.save(b);
+				})
+				.as(StepVerifier::create)
+				.expectNextMatches(a -> a.getRangeRelationsIn().size() == 1 && a.getRangeRelationsOut().size() == 1)
+				.verifyComplete();
+	}
+
+	@Repository
+	public interface SkuRepository extends ReactiveNeo4jRepository<Sku, Long> {
+	}
+
+	@Configuration
+	@EnableTransactionManagement
+	@EnableReactiveNeo4jRepositories(considerNestedRepositories = true)
+	static class Config extends AbstractReactiveNeo4jConfig {
+
+		@Bean
+		public BookmarkCapture bookmarkCapture() {
+			return new BookmarkCapture();
+		}
+
+		@Bean
+		public Driver driver() {
+
+			return neo4jConnectionSupport.getDriver();
+		}
+	}
+}
