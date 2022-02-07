e65fa55f0f03f8e6f01d835e68bfe04e78e00e0d
Michael Simons <michael@simons.ac>
2022-02-07 12:30:00 +0100

    GH-2474 - Allow escaped properties in custom sort order.
    Fixes #2474.

---
+++ src/test/java/org/springframework/data/neo4j/integration/issues/gh2474/GH2474IT.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2011-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.integration.issues.gh2474;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.driver.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.neo4j.config.AbstractNeo4jConfig;
+import org.springframework.data.neo4j.core.DatabaseSelectionProvider;
+import org.springframework.data.neo4j.core.transaction.Neo4jBookmarkManager;
+import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.test.BookmarkCapture;
+import org.springframework.data.neo4j.test.Neo4jExtension;
+import org.springframework.data.neo4j.test.Neo4jIntegrationTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Stephen Jackson
+ * @author Michael J. Simons
+ */
+@Neo4jIntegrationTest
+public class GH2474IT {
+
+	protected static Neo4jExtension.Neo4jConnectionSupport neo4jConnectionSupport;
+
+	@Autowired
+	Driver driver;
+
+	@Autowired
+	BookmarkCapture bookmarkCapture;
+
+	@Autowired
+	CityModelRepository cityModelRepository;
+
+	@BeforeEach
+	void setupData() {
+
+		cityModelRepository.deleteAll();
+
+		CityModel aachen = new CityModel();
+		aachen.setName("Aachen");
+		aachen.setExoticProperty("Cars");
+
+		CityModel utrecht = new CityModel();
+		utrecht.setName("Utrecht");
+		utrecht.setExoticProperty("Bikes");
+
+		cityModelRepository.saveAll(Arrays.asList(aachen, utrecht));
+	}
+
+	@Test
+	public void testStoreExoticProperty() {
+
+		CityModel cityModel = new CityModel();
+		cityModel.setName("The Jungle");
+		cityModel.setExoticProperty("lions");
+		cityModel = cityModelRepository.save(cityModel);
+
+		CityModel reloaded = cityModelRepository.findById(cityModel.getCityId())
+				.orElseThrow(RuntimeException::new);
+		assertThat(reloaded.getExoticProperty()).isEqualTo("lions");
+
+		long cnt = cityModelRepository.deleteAllByExoticProperty("lions");
+		assertThat(cnt).isOne();
+	}
+
+	@Test
+	public void testSortOnExoticProperty() {
+
+		Sort sort = Sort.by(Sort.Order.asc("exoticProperty"));
+		List<CityModel> cityModels = cityModelRepository.findAll(sort);
+
+		assertThat(cityModels).extracting(CityModel::getExoticProperty).containsExactly("Bikes", "Cars");
+	}
+
+	@Test
+	public void testSortOnExoticPropertyCustomQuery_MakeSureIUnderstand() {
+
+		Sort sort = Sort.by(Sort.Order.asc("n.name"));
+		List<CityModel> cityModels = cityModelRepository.customQuery(sort);
+
+		assertThat(cityModels).extracting(CityModel::getExoticProperty).containsExactly("Cars", "Bikes");
+	}
+
+	@Test
+	public void testSortOnExoticPropertyCustomQuery() {
+		Sort sort = Sort.by(Sort.Order.asc("n.`exotic.property`"));
+		List<CityModel> cityModels = cityModelRepository.customQuery(sort);
+
+		assertThat(cityModels).extracting(CityModel::getExoticProperty).containsExactly("Bikes", "Cars");
+	}
+
+	@Configuration
+	@EnableTransactionManagement
+	@EnableNeo4jRepositories
+	static class Config extends AbstractNeo4jConfig {
+
+		@Bean
+		public BookmarkCapture bookmarkCapture() {
+			return new BookmarkCapture();
+		}
+
+		@Override
+		public PlatformTransactionManager transactionManager(
+				Driver driver, DatabaseSelectionProvider databaseNameProvider) {
+
+			BookmarkCapture bookmarkCapture = bookmarkCapture();
+			return new Neo4jTransactionManager(driver, databaseNameProvider,
+					Neo4jBookmarkManager.create(bookmarkCapture));
+		}
+
+		@Bean
+		public Driver driver() {
+
+			return neo4jConnectionSupport.getDriver();
+		}
+	}
+}
