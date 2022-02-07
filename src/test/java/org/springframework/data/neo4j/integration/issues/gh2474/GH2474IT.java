df86b883c49377c509810f02b1551491b6ce2543
Michael Simons <michael@simons.ac>
2022-02-07 19:14:34 +0100

    GH-2475 - Recursively hydrate entities from DTOs.
    This is necessary so that all properties are actually filled before the entity is passed to the persister.
    
    Fixes #2475

--- src/test/java/org/springframework/data/neo4j/integration/issues/gh2474/GH2474IT.java
+++ src/test/java/org/springframework/data/neo4j/integration/issues/gh2474/GH2474IT.java
@@ -18,6 +18,7 @@ package org.springframework.data.neo4j.integration.issues.gh2474;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -29,6 +30,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.neo4j.config.AbstractNeo4jConfig;
 import org.springframework.data.neo4j.core.DatabaseSelectionProvider;
+import org.springframework.data.neo4j.core.Neo4jTemplate;
 import org.springframework.data.neo4j.core.transaction.Neo4jBookmarkManager;
 import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
@@ -55,6 +57,12 @@ public class GH2474IT {
 
 	@Autowired
 	CityModelRepository cityModelRepository;
+
+	@Autowired
+	PersonModelRepository personModelRepository;
+
+	@Autowired
+	Neo4jTemplate neo4jTemplate;
 
 	@BeforeEach
 	void setupData() {
@@ -112,6 +120,42 @@ public class GH2474IT {
 		List<CityModel> cityModels = cityModelRepository.customQuery(sort);
 
 		assertThat(cityModels).extracting(CityModel::getExoticProperty).containsExactly("Bikes", "Cars");
+	}
+
+	@Test // GH-2475
+	public void testCityModelProjectionPersistence() {
+		CityModel cityModel = new CityModel();
+		cityModel.setName("New Cool City");
+		cityModel = cityModelRepository.save(cityModel);
+
+		PersonModel personModel = new PersonModel();
+		personModel.setName("Mr. Mayor");
+		personModel.setAddress("1600 City Avenue");
+		personModel.setFavoriteFood("tacos");
+		personModelRepository.save(personModel);
+
+		CityModelDTO cityModelDTO = cityModelRepository.findByCityId(cityModel.getCityId())
+				.orElseThrow(RuntimeException::new);
+		cityModelDTO.setName("Changed name");
+		cityModelDTO.setExoticProperty("tigers");
+
+		CityModelDTO.PersonModelDTO personModelDTO = new CityModelDTO.PersonModelDTO();
+		personModelDTO.setPersonId(personModelDTO.getPersonId());
+
+		CityModelDTO.JobRelationshipDTO jobRelationshipDTO = new CityModelDTO.JobRelationshipDTO();
+		jobRelationshipDTO.setPerson(personModelDTO);
+
+		cityModelDTO.setMayor(personModelDTO);
+		cityModelDTO.setCitizens(Collections.singletonList(personModelDTO));
+		cityModelDTO.setCityEmployees(Collections.singletonList(jobRelationshipDTO));
+		neo4jTemplate.save(CityModel.class).one(cityModelDTO);
+
+		CityModel reloaded = cityModelRepository.findById(cityModel.getCityId())
+				.orElseThrow(RuntimeException::new);
+		assertThat(reloaded.getName()).isEqualTo("Changed name");
+		assertThat(reloaded.getMayor()).isNotNull();
+		assertThat(reloaded.getCitizens()).hasSize(1);
+		assertThat(reloaded.getCityEmployees()).hasSize(1);
 	}
 
 	@Configuration
