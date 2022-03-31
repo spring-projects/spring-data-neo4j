ed1d47bf372373de10f5589e193b29a49673ed8b
Michael Simons <michael@simons.ac>
2022-03-31 11:16:17 +0200

    GH-2493 - Unwrap `MapValueWrapper` for list parameters, too.
    This fixes #2493.

---
+++ src/test/java/org/springframework/data/neo4j/integration/issues/gh2493/TestObject.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2011-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.integration.issues.gh2493;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.data.neo4j.core.schema.CompositeProperty;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * @author Michael J. Simons
+ */
+@Node
+@Getter
+@Setter
+public class TestObject {
+
+	@Id
+	@Property(name = "id")
+	@GeneratedValue(value = UUIDStringGenerator.class)
+	protected String id;
+
+	@JsonIgnore
+	@CompositeProperty(delimiter = "", converter = TestConverter.class)
+	private TestData data;
+
+	public TestObject(TestData aData) {
+		super();
+		data = aData;
+	}
+}
