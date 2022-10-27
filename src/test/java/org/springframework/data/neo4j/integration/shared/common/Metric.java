985da3af1f53d44dfb43cac289111cdd45a5f7ca
Michael Simons <michael@simons.ac>
2022-10-27 13:20:31 +0200

    GH-2619 - Correctly count most matching, static labels.
    In case another matching node description is found, the value of the `mostMatchingStaticLabels` must be updated.
    
    Fixes #2619.

---
+++ src/test/java/org/springframework/data/neo4j/integration/shared/common/Metric.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2011-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.integration.shared.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.neo4j.core.schema.DynamicLabels;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+/**
+ * @author Michael J. Simons
+ */
+@Node("Metric")
+public abstract class Metric {
+
+	@Id
+	@GeneratedValue
+	Long id;
+
+	@DynamicLabels
+	public List<String> dynamicLabels = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public List<String> getDynamicLabels() {
+		return dynamicLabels;
+	}
+
+	private String name;
+
+	public Metric(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
