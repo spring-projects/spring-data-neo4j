5c7ab59433341b54f1675369900335c5b34daed1
Michael Simons <michael@simons.ac>
2023-01-05 10:56:02 +0100

    GH-2640 - Introduce API for dynamic configuration of transient properties. (#2645)
    This change introduces the concept of `PersistentPropertyCharacteristics` and `PersistentPropertyCharacteristicsProvider`.
    The latter can be registered implicitly as a `@Bean` with the mapping context or via a custom mapping context. It allows
    checking the properties and their owner for indicators like type and so on to treat them as transient or read only.
    Examples have been added to the `Neo4jMappingContextTest` and `PersistentPropertyCharacteristicsIT`.
    
    This closes #2640.

---
+++ src/main/java/org/springframework/data/neo4j/core/mapping/PersistentPropertyCharacteristics.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2011-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.neo4j.core.mapping;
+
+import static org.apiguardian.api.API.Status.STABLE;
+
+import org.apiguardian.api.API;
+import org.springframework.lang.Nullable;
+
+/**
+ * The characteristics of a {@link Neo4jPersistentProperty} can diverge from what is by default derived from the annotated
+ * classes. Diverging characteristics are requested by the {@link Neo4jMappingContext} prior to creating a persistent property.
+ * Additional providers of characteristics may be registered with the mapping context.
+ *
+ * @author Michael J. Simons
+ * @soundtrack Metallica - Kill 'Em All
+ * @since 6.3.7
+ */
+@API(status = STABLE, since = "6.3.7")
+public interface PersistentPropertyCharacteristics {
+
+	/**
+	 * @return {@literal null} to leave the defaults, {@literal true} or {@literal false} otherwise
+	 */
+	@Nullable
+	default Boolean isTransient() {
+		return null;
+	}
+
+	/**
+	 * @return {@literal null} to leave the defaults, {@literal true} or {@literal false} otherwise
+	 */
+	@Nullable
+	default Boolean isReadOnly() {
+		return null;
+	}
+
+	/**
+	 * @return Characteristics applying the defaults
+	 */
+	static PersistentPropertyCharacteristics useDefaults() {
+		return new PersistentPropertyCharacteristics() {
+		};
+	}
+
+	/**
+	 * @return Characteristics to treat a property as transient
+	 */
+	static PersistentPropertyCharacteristics treatAsTransient() {
+		return new PersistentPropertyCharacteristics() {
+			@Override
+			public Boolean isTransient() {
+				return true;
+			}
+		};
+	}
+
+	/**
+	 * @return Characteristics to treat a property as transient
+	 */
+	static PersistentPropertyCharacteristics treatAsReadOnly() {
+		return new PersistentPropertyCharacteristics() {
+			@Override
+			public Boolean isReadOnly() {
+				return true;
+			}
+		};
+	}
+}
