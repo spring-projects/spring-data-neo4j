0326bff379066c560e0c6e00210de147aa813341
Gerrit Meier <meistermeier@gmail.com>
2023-06-06 10:03:25 +0200

    Adjust test log filter list.

--- src/test/java/org/springframework/data/neo4j/test/TestLogFilter.java
+++ src/test/java/org/springframework/data/neo4j/test/TestLogFilter.java
@@ -28,7 +28,9 @@ public class TestLogFilter extends Filter<ILoggingEvent> {
 
 	private static final List<String> FILTER_MESSAGES = List.of(
 			"ClientNotification.Statement.UnknownRelationshipTypeWarning",
-			"ClientNotification.Statement.UnknownPropertyKeyWarning"
+			"ClientNotification.Statement.UnknownLabelWarning",
+			"ClientNotification.Statement.UnknownPropertyKeyWarning",
+			"ClientNotification.Statement.FeatureDeprecationWarning"
 	);
 
 	@Override
