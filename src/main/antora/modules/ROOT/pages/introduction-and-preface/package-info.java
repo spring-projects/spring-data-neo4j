979c568704849f92c34c1df73ea5472c043fa796
Gerrit Meier <meistermeier@gmail.com>
2023-09-06 12:58:59 +0200

    Build setup and first section: preface.

---
+++ src/main/antora/modules/ROOT/pages/introduction-and-preface/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <!-- tag::intent[] -->
+   This package contains configuration related support classes that can be used for application specific, annotated
+   configuration classes. The abstract base classes are helpful if you don't rely on Spring Boot's autoconfiguration.
+   The package provides some additional annotations that enable auditing.
+ * <!-- end::intent[] -->
+ */
+@NonNullApi
+package org.springframework.data.neo4j.config;
+
+import org.springframework.lang.NonNullApi;
