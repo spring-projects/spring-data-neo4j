279ba10388e3d1c11ba1d4d2f6e962bbfcbbafd9
Michael Simons <michael@simons.ac>
2023-11-28 12:26:25 +0100

    Allow enforcement to reuse a container in the CI build.
    This orders reusable containers on wish and is meant for scenarios in which a CI does not allow "native" use of reusable test containers but will make sure to clean them up after CI ran. Export a variable `SDN_FORCE_REUSE_OF_CONTAINERS` as `true` and SDN tests won't close the container, regardless whether the underlying Testcontainers supports reuse or not.
    
    Closes #2837

--- ci/test.sh
+++ ci/test.sh
@@ -6,6 +6,7 @@ mkdir -p /tmp/jenkins-home
 chown -R 1001:1001 .
 
 export JENKINS_USER=${JENKINS_USER_NAME}
+export SDN_FORCE_REUSE_OF_CONTAINERS=true
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home -Dscan=false" \
   ./mvnw -s settings.xml -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-neo4j
