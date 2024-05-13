0b5a51219d5c3518e7c6c2d3ab2bddbc10f9f4c8
Michael Simons <michael@simons.ac>
2024-05-13 15:38:02 +0200

    fix: Load properties required to instantiate KClasses backing projections proper.
    Closes #2899

---
+++ src/test/kotlin/org/springframework/data/neo4j/integration/k/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Add `k` as package name as I couldn't figure out in which way the presence of KotlinIssuesIT in the split package
+ * (between the Kotlin and Java code) messed up the application context in tests.
+ */
+package org.springframework.data.neo4j.integration.k;
