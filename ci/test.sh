e8de22e9fd877419337b3e6f989342c510efe6b0
Michael Simons <michael@simons.ac>
2024-07-09 11:17:17 +0200

    build: Pin CI Neo4j version to 5.20.

--- ci/test.sh
+++ ci/test.sh
@@ -7,6 +7,7 @@ chown -R 1001:1001 .
 
 export JENKINS_USER=${JENKINS_USER_NAME}
 export SDN_FORCE_REUSE_OF_CONTAINERS=true
+export SDN_NEO4J_VERSION=5.20
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home -Dscan=false" \
   ./mvnw -s settings.xml -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-neo4j
