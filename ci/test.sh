720d1632995ea8171b7577ee915c248e01acf6f4
Eric Haag <ehaag@gradle.com>
2024-08-01 08:43:44 -0500

    Migrate build to Spring Develocity Conventions extension.
    * Migrate build to Spring Develocity Conventions extension.
    
    * Adopt Develocity environment variables.
    
    Closes #2929

--- ci/test.sh
+++ ci/test.sh
@@ -5,14 +5,9 @@ set -euo pipefail
 mkdir -p /tmp/jenkins-home
 chown -R 1001:1001 .
 
-export DEVELOCITY_CACHE_USERNAME=${DEVELOCITY_CACHE_USR}
-export DEVELOCITY_CACHE_PASSWORD=${DEVELOCITY_CACHE_PSW}
 export JENKINS_USER=${JENKINS_USER_NAME}
 export SDN_FORCE_REUSE_OF_CONTAINERS=true
 export SDN_NEO4J_VERSION=5.20
-
-# The environment variable to configure access key is still GRADLE_ENTERPRISE_ACCESS_KEY
-export GRADLE_ENTERPRISE_ACCESS_KEY=${DEVELOCITY_ACCESS_KEY}
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home -Dscan=false" \
   ./mvnw -s settings.xml -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-neo4j
