0d331bd9cf332be1c581c8f1ca74f530531c4946
Mark Paluch <mark.paluch@broadcom.com>
2024-10-09 09:05:45 +0200

    Consistently run all CI steps with the same user.
    See #2951

--- ci/test.sh
+++ ci/test.sh
@@ -3,11 +3,10 @@
 set -euo pipefail
 
 mkdir -p /tmp/jenkins-home
-chown -R 1001:1001 .
 
 export JENKINS_USER=${JENKINS_USER_NAME}
 export SDN_FORCE_REUSE_OF_CONTAINERS=true
 export SDN_NEO4J_VERSION=5.20
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home -Dscan=false" \
-  ./mvnw -s settings.xml -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-neo4j
+  ./mvnw -s settings.xml -P${PROFILE} clean dependency:list verify -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-neo4j -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
