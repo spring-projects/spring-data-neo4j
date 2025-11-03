f07d36583318c57041b2f77249cf48dae493403b
Gerrit Meier <meistermeier@gmail.com>
2025-11-03 15:53:00 +0100

    Fix nullaway problems that appeared after reactor upgrade. (#3057)
    Co-authored-by: Michael Simons <michael@simons.ac>
    Signed-off-by: Gerrit Meier <meistermeier@gmail.com>

--- src/main/java/org/springframework/data/neo4j/core/ReactiveNeo4jTemplate.java
+++ src/main/java/org/springframework/data/neo4j/core/ReactiveNeo4jTemplate.java
@@ -566,7 +566,8 @@ public final class ReactiveNeo4jTemplate
 				.one()
 				.map(m -> (Collection<String>) m.get(Constants.NAME_OF_LABELS))
 				.switchIfEmpty(Mono.just(Collections.emptyList()))
-				.zipWith(Mono.just((Collection<String>) propertyAccessor.getProperty(p)))
+				.zipWith(Mono.just(
+						(Collection<String>) Objects.requireNonNullElseGet(propertyAccessor.getProperty(p), List::of)))
 				.map(t -> Tuples.of(entityToBeSaved, new DynamicLabels(entityMetaData, t.getT1(), t.getT2())));
 		}).orElse(Mono.just(Tuples.of(entityToBeSaved, DynamicLabels.EMPTY)));
 	}
